ea6a18c6b8a591f857500e93182d3eac748f693b
Afforess <afforess@gmail.com>
2011-08-04 19:34:10 -0400

    Render per pixel

--- src/minecraft/net/minecraft/src/TileEntityRenderer.java
+++ src/minecraft/net/minecraft/src/TileEntityRenderer.java
@@ -1,137 +1,25 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package net.minecraft.src;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import net.minecraft.src.EntityLiving;
+import net.minecraft.src.FontRenderer;
+import net.minecraft.src.RenderEngine;
+import net.minecraft.src.TileEntity;
+import net.minecraft.src.TileEntityMobSpawner;
+import net.minecraft.src.TileEntityMobSpawnerRenderer;
+import net.minecraft.src.TileEntityPiston;
+import net.minecraft.src.TileEntityRendererPiston;
+import net.minecraft.src.TileEntitySign;
+import net.minecraft.src.TileEntitySignRenderer;
+import net.minecraft.src.TileEntitySpecialRenderer;
+import net.minecraft.src.World;
 import org.lwjgl.opengl.GL11;
 
-// Referenced classes of package net.minecraft.src:
-//			TileEntitySign, TileEntitySignRenderer, TileEntityMobSpawner, TileEntityMobSpawnerRenderer, 
-//			TileEntityPiston, TileEntityRendererPiston, TileEntitySpecialRenderer, TileEntity, 
-//			EntityLiving, World, FontRenderer, RenderEngine
+public class TileEntityRenderer {
 
-public class TileEntityRenderer
-{
-
-	private TileEntityRenderer()
-	{
-		specialRendererMap = new HashMap();
-		specialRendererMap.put(net.minecraft.src.TileEntitySign.class, new TileEntitySignRenderer());
-		specialRendererMap.put(net.minecraft.src.TileEntityMobSpawner.class, new TileEntityMobSpawnerRenderer());
-		specialRendererMap.put(net.minecraft.src.TileEntityPiston.class, new TileEntityRendererPiston());
-		TileEntitySpecialRenderer tileentityspecialrenderer;
-		for(Iterator iterator = specialRendererMap.values().iterator(); iterator.hasNext(); tileentityspecialrenderer.setTileEntityRenderer(this))
-		{
-			tileentityspecialrenderer = (TileEntitySpecialRenderer)iterator.next();
-		}
-
-	}
-
-	public TileEntitySpecialRenderer getSpecialRendererForClass(Class class1)
-	{
-		TileEntitySpecialRenderer tileentityspecialrenderer = (TileEntitySpecialRenderer)specialRendererMap.get(class1);
-		if(tileentityspecialrenderer == null && class1 != (net.minecraft.src.TileEntity.class))
-		{
-			tileentityspecialrenderer = getSpecialRendererForClass(class1.getSuperclass());
-			specialRendererMap.put(class1, tileentityspecialrenderer);
-		}
-		return tileentityspecialrenderer;
-	}
-
-	public boolean hasSpecialRenderer(TileEntity tileentity)
-	{
-		return getSpecialRendererForEntity(tileentity) != null;
-	}
-
-	public TileEntitySpecialRenderer getSpecialRendererForEntity(TileEntity tileentity)
-	{
-		if(tileentity == null)
-		{
-			return null;
-		} else
-		{
-			return getSpecialRendererForClass(tileentity.getClass());
-		}
-	}
-
-	public void cacheActiveRenderInfo(World world, RenderEngine renderengine, FontRenderer fontrenderer, EntityLiving entityliving, float f)
-	{
-		if(worldObj != world)
-		{
-			func_31072_a(world);
-		}
-		renderEngine = renderengine;
-		entityLivingPlayer = entityliving;
-		fontRenderer = fontrenderer;
-		playerYaw = entityliving.prevRotationYaw + (entityliving.rotationYaw - entityliving.prevRotationYaw) * f;
-		playerPitch = entityliving.prevRotationPitch + (entityliving.rotationPitch - entityliving.prevRotationPitch) * f;
-		playerX = entityliving.lastTickPosX + (entityliving.posX - entityliving.lastTickPosX) * (double)f;
-		playerY = entityliving.lastTickPosY + (entityliving.posY - entityliving.lastTickPosY) * (double)f;
-		playerZ = entityliving.lastTickPosZ + (entityliving.posZ - entityliving.lastTickPosZ) * (double)f;
-	}
-
-	public void renderTileEntity(TileEntity tileentity, float f)
-	{
-		if(tileentity.getDistanceFrom(playerX, playerY, playerZ) < 4096D)
-		{
-			float f1 = worldObj.getLightBrightness(tileentity.xCoord, tileentity.yCoord, tileentity.zCoord);
-			GL11.glColor3f(f1, f1, f1);
-			renderTileEntityAt(tileentity, (double)tileentity.xCoord - staticPlayerX, (double)tileentity.yCoord - staticPlayerY, (double)tileentity.zCoord - staticPlayerZ, f);
-		}
-	}
-
-	public void renderTileEntityAt(TileEntity tileentity, double d, double d1, double d2, 
-			float f)
-	{
-		TileEntitySpecialRenderer tileentityspecialrenderer = getSpecialRendererForEntity(tileentity);
-		if(tileentityspecialrenderer != null)
-		{
-			tileentityspecialrenderer.renderTileEntityAt(tileentity, d, d1, d2, f);
-		}
-	}
-
-	public void func_31072_a(World world)
-	{
-		worldObj = world;
-		Iterator iterator = specialRendererMap.values().iterator();
-		do
-		{
-			if(!iterator.hasNext())
-			{
-				break;
-			}
-			TileEntitySpecialRenderer tileentityspecialrenderer = (TileEntitySpecialRenderer)iterator.next();
-			if(tileentityspecialrenderer != null)
-			{
-				tileentityspecialrenderer.func_31069_a(world);
-			}
-		} while(true);
-	}
-
-	public FontRenderer getFontRenderer()
-	{
-		return fontRenderer;
-	}
-
-	//Spout Start
-	public void clear() {
-		worldObj = null;
-		entityLivingPlayer = null;
-		specialRendererMap = new HashMap();
-		specialRendererMap.put(net.minecraft.src.TileEntitySign.class, new TileEntitySignRenderer());
-		specialRendererMap.put(net.minecraft.src.TileEntityMobSpawner.class, new TileEntityMobSpawnerRenderer());
-		specialRendererMap.put(net.minecraft.src.TileEntityPiston.class, new TileEntityRendererPiston());
-		TileEntitySpecialRenderer tileentityspecialrenderer;
-		for(Iterator iterator = specialRendererMap.values().iterator(); iterator.hasNext(); tileentityspecialrenderer.setTileEntityRenderer(this))
-		{
-			tileentityspecialrenderer = (TileEntitySpecialRenderer)iterator.next();
-		}
-	}
-	//Spout End
-
-	private Map specialRendererMap;
+	private Map specialRendererMap = new HashMap();
 	public static TileEntityRenderer instance = new TileEntityRenderer();
 	private FontRenderer fontRenderer;
 	public static double staticPlayerX;
@@ -145,5 +33,104 @@ public class TileEntityRenderer
 	public double playerX;
 	public double playerY;
 	public double playerZ;
+
+
+	private TileEntityRenderer() {
+		this.specialRendererMap.put(TileEntitySign.class, new TileEntitySignRenderer());
+		this.specialRendererMap.put(TileEntityMobSpawner.class, new TileEntityMobSpawnerRenderer());
+		this.specialRendererMap.put(TileEntityPiston.class, new TileEntityRendererPiston());
+		Iterator var1 = this.specialRendererMap.values().iterator();
+
+		while(var1.hasNext()) {
+			TileEntitySpecialRenderer var2 = (TileEntitySpecialRenderer)var1.next();
+			var2.setTileEntityRenderer(this);
+		}
+
+	}
+	
+	//Spout start
+	public void clear() {
+		worldObj = null;
+		entityLivingPlayer = null;
+		specialRendererMap.clear();
+		
+		this.specialRendererMap.put(TileEntitySign.class, new TileEntitySignRenderer());
+		this.specialRendererMap.put(TileEntityMobSpawner.class, new TileEntityMobSpawnerRenderer());
+		this.specialRendererMap.put(TileEntityPiston.class, new TileEntityRendererPiston());
+		Iterator var1 = this.specialRendererMap.values().iterator();
+
+		while(var1.hasNext()) {
+			TileEntitySpecialRenderer var2 = (TileEntitySpecialRenderer)var1.next();
+			var2.setTileEntityRenderer(this);
+		}
+	}
+	//Spout end
+
+	public TileEntitySpecialRenderer getSpecialRendererForClass(Class var1) {
+		TileEntitySpecialRenderer var2 = (TileEntitySpecialRenderer)this.specialRendererMap.get(var1);
+		if(var2 == null && var1 != TileEntity.class) {
+			var2 = this.getSpecialRendererForClass(var1.getSuperclass());
+			this.specialRendererMap.put(var1, var2);
+		}
+
+		return var2;
+	}
+
+	public boolean hasSpecialRenderer(TileEntity var1) {
+		return this.getSpecialRendererForEntity(var1) != null;
+	}
+
+	public TileEntitySpecialRenderer getSpecialRendererForEntity(TileEntity var1) {
+		return var1 == null?null:this.getSpecialRendererForClass(var1.getClass());
+	}
+
+	public void cacheActiveRenderInfo(World var1, RenderEngine var2, FontRenderer var3, EntityLiving var4, float var5) {
+		if(this.worldObj != var1) {
+			this.func_31072_a(var1);
+		}
+
+		this.renderEngine = var2;
+		this.entityLivingPlayer = var4;
+		this.fontRenderer = var3;
+		this.playerYaw = var4.prevRotationYaw + (var4.rotationYaw - var4.prevRotationYaw) * var5;
+		this.playerPitch = var4.prevRotationPitch + (var4.rotationPitch - var4.prevRotationPitch) * var5;
+		this.playerX = var4.lastTickPosX + (var4.posX - var4.lastTickPosX) * (double)var5;
+		this.playerY = var4.lastTickPosY + (var4.posY - var4.lastTickPosY) * (double)var5;
+		this.playerZ = var4.lastTickPosZ + (var4.posZ - var4.lastTickPosZ) * (double)var5;
+	}
+
+	public void renderTileEntity(TileEntity var1, float var2) {
+		if(var1.getDistanceFrom(this.playerX, this.playerY, this.playerZ) < 4096.0D) {
+			float var3 = this.worldObj.getLightBrightness(var1.xCoord, var1.yCoord, var1.zCoord);
+			GL11.glColor3f(var3, var3, var3);
+			this.renderTileEntityAt(var1, (double)var1.xCoord - staticPlayerX, (double)var1.yCoord - staticPlayerY, (double)var1.zCoord - staticPlayerZ, var2);
+		}
+
+	}
+
+	public void renderTileEntityAt(TileEntity var1, double var2, double var4, double var6, float var8) {
+		TileEntitySpecialRenderer var9 = this.getSpecialRendererForEntity(var1);
+		if(var9 != null) {
+			var9.renderTileEntityAt(var1, var2, var4, var6, var8);
+		}
+
+	}
+
+	public void func_31072_a(World var1) {
+		this.worldObj = var1;
+		Iterator var2 = this.specialRendererMap.values().iterator();
+
+		while(var2.hasNext()) {
+			TileEntitySpecialRenderer var3 = (TileEntitySpecialRenderer)var2.next();
+			if(var3 != null) {
+				var3.func_31069_a(var1);
+			}
+		}
+
+	}
+
+	public FontRenderer getFontRenderer() {
+		return this.fontRenderer;
+	}
 
 }
