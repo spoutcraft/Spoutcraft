f0716d8103824d357cab116076e4c29e9b55315e
Afforess <afforess@gmail.com>
2011-08-05 11:56:38 -0300

    Security Update. Reject invalid audio files.

--- src/minecraft/net/minecraft/src/org/getspout/spout/packet/PacketDownloadMusic.java
+++ src/minecraft/net/minecraft/src/org/getspout/spout/packet/PacketDownloadMusic.java
@@ -14,19 +14,19 @@ public class PacketDownloadMusic implements SpoutPacket{
 	int x, y, z;
 	int volume, distance;
 	boolean soundEffect, notify;
-	String URL, plugin;
+	String url, plugin;
 	public PacketDownloadMusic() {
 		
 	}
 
 	@Override
 	public int getNumBytes() {
-		return 22 + PacketUtil.getNumBytes(URL) + PacketUtil.getNumBytes(plugin);
+		return 22 + PacketUtil.getNumBytes(url) + PacketUtil.getNumBytes(plugin);
 	}
 
 	@Override
 	public void readData(DataInputStream input) throws IOException {
-		URL = PacketUtil.readString(input, 255);
+		url = PacketUtil.readString(input, 255);
 		plugin = PacketUtil.readString(input, 255);
 		distance = input.readInt();
 		x = input.readInt();
@@ -39,7 +39,7 @@ public class PacketDownloadMusic implements SpoutPacket{
 
 	@Override
 	public void writeData(DataOutputStream output) throws IOException {
-		PacketUtil.writeString(output, URL);
+		PacketUtil.writeString(output, url);
 		PacketUtil.writeString(output, plugin);
 		output.writeInt(distance);
 		output.writeInt(x);
@@ -57,23 +57,19 @@ public class PacketDownloadMusic implements SpoutPacket{
 		if (!directory.exists()){	
 			directory.mkdir();
 		}
-		File song = new File(directory, getFileName());
+		String fileName = FileUtil.getFileName(url);
+		if (!FileUtil.isAudioFile(fileName)) {
+			System.out.println("Rejecting download of invalid audio: " + fileName);
+			return;
+		}
+		File song = new File(directory, fileName);
 		QueuedSound action = new QueuedSound(song, x, y, z, volume, distance, soundEffect);
-		Download download = new Download(getFileName(), directory, URL, action);
+		Download download = new Download(fileName, directory, URL, action);
 		action.setNotify(!download.isDownloaded() && notify);
 		if (!download.isDownloaded() && notify) {
-			Spout.createSpoutAlert("Downloading Music...", getFileName(), 2256 /*Gold Record*/);
+			Spout.createSpoutAlert("Downloading Music...", fileName, 2256 /*Gold Record*/);
 		}
 		FileDownloadThread.getInstance().addToDownloadQueue(download);
-	}
-	
-	private String getFileName() {
-		int slashIndex = URL.lastIndexOf('/');
-		int dotIndex = URL.lastIndexOf('.', slashIndex);
-		if (dotIndex == -1 || dotIndex < slashIndex) {
-				return URL.substring(slashIndex + 1).replaceAll("%20", " ");
-		}
-		return URL.substring(slashIndex + 1, dotIndex).replaceAll("%20", " ");
 	}
 
 	@Override
