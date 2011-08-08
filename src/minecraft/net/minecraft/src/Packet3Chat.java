d78b5c19d9012b1a61eddfe3e162e8666fbee414
Afforess <afforess@gmail.com>
2011-08-08 11:56:41 -0300

    Cleanup

--- src/minecraft/net/minecraft/src/Packet3Chat.java
+++ src/minecraft/net/minecraft/src/Packet3Chat.java
@@ -5,12 +5,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import net.minecraft.src.NetHandler;
 import net.minecraft.src.Packet;
-//Spout Start
-import net.minecraft.client.Minecraft;
-
-import org.getspout.spout.client.SpoutClient;
-import org.getspout.spout.packet.*;
-//Spout End
 
 public class Packet3Chat extends Packet {
 
@@ -36,36 +30,6 @@ public class Packet3Chat extends Packet {
 	}
 
 	public void processPacket(NetHandler nethandler) {
-		//Spout Start
-		/*boolean proc = false;
-		if (!SpoutClient.getInstance().isSpoutEnabled() || SpoutClient.getReloadPacket() != null) {
-			String processed = Spout.colorToString(message);
-			System.out.println(processed);
-			if (processed.split("\\.").length == 3) {
-				Spout.setVersion(processed);
-				if (Spout.isEnabled()) {
-					proc = true;
-					System.out.println("Spout SP Enabled");
-					((NetClientHandler)nethandler).addToSendQueue(new Packet3Chat("/" + Spout.getClientVersionString()));
-					//Let Spout know we just reloaded
-					if (Spout.getReloadPacket() != null) {
-						((NetClientHandler)nethandler).addToSendQueue(new CustomPacket(Spout.getReloadPacket()));
-						Spout.setReloadPacket(null);
-					}
-					//Also need to send the render distance
-					Minecraft game = SpoutClient.getHandle();
-					if (game != null && Spout.getVersion() > 5) {
-						final GameSettings settings = game.gameSettings;
-						((NetClientHandler)nethandler).addToSendQueue(new CustomPacket(new PacketRenderDistance((byte)settings.renderDistance)));
-					}
-				}
-			}
-		}
-		if (!proc) {
-			//Normal message handling
-			nethandler.handleChat(this);
-		}*/
-		//Spout End
 		nethandler.handleChat(this);
 	}
 
