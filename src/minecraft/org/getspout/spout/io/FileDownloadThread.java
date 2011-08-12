520352328be6e46443ee9eab23c6dc2247022510
raphfrk <raphfrk@gmail.com>
2011-08-12 21:05:55 +0100

    Progress indication when downloading files

--- src/minecraft/org/getspout/spout/io/FileDownloadThread.java
+++ src/minecraft/org/getspout/spout/io/FileDownloadThread.java
@@ -1,6 +1,8 @@
 package org.getspout.spout.io;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.Iterator;
@@ -10,6 +12,7 @@ public class FileDownloadThread extends Thread{
 	private static FileDownloadThread instance = null;
 	private final ConcurrentLinkedQueue<Download> downloads = new ConcurrentLinkedQueue<Download>();
 	private final ConcurrentLinkedQueue<Runnable> actions = new ConcurrentLinkedQueue<Runnable>();
+	private final byte[] buffer = new byte[1024*1024];
 	
 	protected FileDownloadThread() {
 		super("File Download Thread");
@@ -55,11 +58,36 @@ public class FileDownloadThread extends Thread{
 					if (!next.isDownloaded()) {
 						System.out.println("Downloading File: " + next.getDownloadUrl());
 						URL url = new URL(next.getDownloadUrl());
-						ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+						URLConnection conn = url.openConnection();
+						InputStream in = conn.getInputStream();
+						
 						FileOutputStream fos = new FileOutputStream(next.getTempFile());
-						fos.getChannel().transferFrom(rbc, 0, 1 << 28);
+						
+						long length = conn.getContentLength();
+						int bytes;
+						long totalBytes = 0;
+						long last = 0;
+						
+						long step = Math.max(1024*1024, length / 8);
+						
+						while ((bytes = in.read(buffer)) >= 0) {
+							fos.write(buffer, 0, bytes);
+							totalBytes += bytes;
+							if (length > 0 && totalBytes > (last + step)) {
+								last = totalBytes;
+								long mb = totalBytes/(1024*1024);
+								System.out.println("Downloading File: " + next.getDownloadUrl() + " " + mb + "MB/" + (length/(1024*1024)));
+							}
+							try {
+								Thread.sleep(100);
+							} catch (InterruptedException e) {
+								
+							}
+						}
+						in.close();
 						fos.close();
 						next.move();
+						System.out.println("File moved to: " + next.directory.getCanonicalPath());
 						try {
 							sleep(10); //cool off after heavy network useage
 						} catch (InterruptedException e) {}
