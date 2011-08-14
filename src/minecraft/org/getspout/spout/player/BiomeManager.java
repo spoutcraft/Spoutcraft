f17d8dd82b22c0a3fe758ca3bb453350a89986dd
Dalton Pelc <djpelc@mtu.edu>
2011-08-14 17:55:29 -0400

    test commit

---
+++ src/minecraft/org/getspout/spout/player/BiomeManager.java
@@ -0,0 +1,13 @@
+package org.getspout.spout.player;
+
+public interface BiomeManager {
+	
+	public void setSnowEnabled();
+	
+	public void setRainEnabled();
+	
+	public boolean getSnowEnabled();
+	
+	public boolean getRainEnabled();
+
+}
