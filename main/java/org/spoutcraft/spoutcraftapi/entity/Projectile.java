6f98e024926c34b1af0f59b9215b48cd9f649cb9
narrowtux <narrow.m@gmail.com>
2011-09-02 00:30:21 +0200

    Remove unused import

--- main/java/org/spoutcraft/spoutcraftapi/entity/Projectile.java
+++ main/java/org/spoutcraft/spoutcraftapi/entity/Projectile.java
@@ -1,7 +1,5 @@
 package org.spoutcraft.spoutcraftapi.entity;
 
-import com.sun.xml.internal.bind.v2.model.core.PropertyInfo;
-
 public interface Projectile extends Entity {
 
 	public LivingEntity getShooter();
