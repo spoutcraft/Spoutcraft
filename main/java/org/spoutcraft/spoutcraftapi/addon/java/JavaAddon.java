67a8881d812e6d03ffe6904f084b86aecaf93bf7
alta189 <stephendwilliams7@gmail.com>
2011-09-01 20:11:30 -0400

    Finished Addon Loading

--- main/java/org/spoutcraft/spoutcraftapi/addon/java/JavaAddon.java
+++ main/java/org/spoutcraft/spoutcraftapi/addon/java/JavaAddon.java
@@ -8,18 +8,48 @@ import org.spoutcraft.spoutcraftapi.addon.AddonDescriptionFile;
 import org.spoutcraft.spoutcraftapi.addon.AddonLoader;
 
 public abstract class JavaAddon implements Addon{
-	
+
+    private boolean initialized = false;
+    private AddonLoader loader = null;
+    private Spoutcraft spoutcraft = null;
+    private File file = null;
+    private File dataFolder = null;
+    private AddonClassLoader classLoader = null;
 	private boolean enabled = false;
 	private AddonDescriptionFile description = null;
-	private AddonLoader loader = null;
+	private boolean naggable = false;
 	
 	public AddonDescriptionFile getDescription() {
 		return description;
+	}
+	
+	public void initialize(JavaAddonLoader loader, Spoutcraft spoutcraft, AddonDescriptionFile description, File dataFolder, File file, AddonClassLoader classLoader) {
+		if (!initialized) {
+            this.initialized = true;
+            this.loader = loader;
+            this.spoutcraft = spoutcraft;
+            this.file = file;
+            this.description = description;
+            this.dataFolder = dataFolder;
+            this.classLoader = classLoader;
+        }
 	}
 
 	public abstract void onEnable();
 
 	public abstract void onDisable();
+	
+	public File getFile() {
+		return file;
+	}
+	
+	public File getDataFolder() {
+		return dataFolder;
+	}
+	
+	public Spoutcraft getSpoutcraft() {
+		return spoutcraft;
+	}
 
 	public AddonLoader getAddonLoader() {
 		return loader;
@@ -39,15 +69,19 @@ public abstract class JavaAddon implements Addon{
 			}
 		}
 	}
+	
+	public final boolean isNaggable() {
+        return naggable;
+    }
 
-	public void initialize(JavaAddonLoader javaAddonLoader, Spoutcraft spoutcraft, AddonDescriptionFile description2, File dataFolder, File file, AddonClassLoader loader2) {
-		// TODO Auto-generated method stub
-		
-	}
+    public final void setNaggable(boolean canNag) {
+        this.naggable = canNag;
+    }
+
+	
 
 	public AddonClassLoader getClassLoader() {
-		// TODO Auto-generated method stub
-		return null;
+		return classLoader;
 	}
 	
 }
