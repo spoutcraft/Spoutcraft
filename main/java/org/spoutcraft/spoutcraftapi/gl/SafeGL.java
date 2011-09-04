fc085a656b8b9cbce80089d22a1771cd635d6ac8
narrowtux <narrow.m@gmail.com>
2011-09-04 18:22:40 +0200

    Oops

--- main/java/org/spoutcraft/spoutcraftapi/gl/SafeGL.java
+++ main/java/org/spoutcraft/spoutcraftapi/gl/SafeGL.java
@@ -2,6 +2,6 @@ package org.spoutcraft.spoutcraftapi.gl;
 
 public interface SafeGL {
 	public void pushMatrix();
-	public void pullMatrix();
+	public void popMatrix();
 	//And all the other methods that are important.
 }
