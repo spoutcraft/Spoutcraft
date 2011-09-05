9a134ae5a59085fd7a3cc1f6dabad7c5821fcb48
alta189 <stephendwilliams7@gmail.com>
2011-09-04 20:27:23 -0400

    Merging Spoutcraft Client with SpoutcraftAPI Client

--- main/java/org/spoutcraft/spoutcraftapi/Client.java
+++ main/java/org/spoutcraft/spoutcraftapi/Client.java
@@ -6,7 +6,12 @@ import java.util.logging.Logger;
 import org.spoutcraft.spoutcraftapi.addon.AddonManager;
 import org.spoutcraft.spoutcraftapi.command.AddonCommand;
 import org.spoutcraft.spoutcraftapi.command.CommandSender;
+import org.spoutcraft.spoutcraftapi.entity.EntityManager;
 import org.spoutcraft.spoutcraftapi.gl.SafeGL;
+import org.spoutcraft.spoutcraftapi.inventory.ItemManager;
+import org.spoutcraft.spoutcraftapi.packet.PacketManager;
+import org.spoutcraft.spoutcraftapi.player.BiomeManager;
+import org.spoutcraft.spoutcraftapi.player.SkyManager;
 
 public interface Client {
 
@@ -30,7 +35,31 @@ public interface Client {
 	
 	public SafeGL getGLWrapper();
 	
+	public ItemManager getItemManager();
+	
+	public SkyManager getSkyManager();
+	
+	public PacketManager getPacketManager();
+	
+	//public ActivePlayer getActivePlayer();
+	
+	public EntityManager getEntityManager();
+	
+	public BiomeManager getBiomeManager();
+	
+	public boolean isCheatMode();
+	
+	public boolean isSpoutEnabled();
+	
+	//public SpoutVersion getServerVersion();
+	
+	//public EntityPlayer getPlayerFromId(int id);
+	
+	public long getTick();
+	
 	public Mode getMode();
+	
+	
 	
 	public enum Mode{
 		Single_Player, Multiplayer, Menu;
---
+++ main/java/org/spoutcraft/spoutcraftapi/packet/PacketManager.java
@@ -0,0 +1,5 @@
+package org.spoutcraft.spoutcraftapi.packet;
+
+public interface PacketManager {
+
+}
