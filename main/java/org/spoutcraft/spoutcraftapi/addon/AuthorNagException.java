3f4156977561f1b80f464e39d815d0e64efcfa9e
alta189 <stephendwilliams7@gmail.com>
2011-09-04 22:17:48 -0400

    Added serial version UID

--- main/java/org/spoutcraft/spoutcraftapi/addon/AuthorNagException.java
+++ main/java/org/spoutcraft/spoutcraftapi/addon/AuthorNagException.java
@@ -1,6 +1,7 @@
 package org.spoutcraft.spoutcraftapi.addon;
 
 public class AuthorNagException extends RuntimeException {
+	private static final long serialVersionUID = 7865800524327635948L;
 	private final String message;
 
 	/**
