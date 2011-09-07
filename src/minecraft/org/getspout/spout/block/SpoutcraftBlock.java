ded609613c3d57360df4ad1c8853feb66a56c646
Top-Cat <Top_Cat@thegigcast.net>
2011-09-07 19:07:46 +0100

    And Top_Cat said: Let there be SUCCESSFUL builds!

--- src/minecraft/org/getspout/spout/block/SpoutcraftBlock.java
+++ src/minecraft/org/getspout/spout/block/SpoutcraftBlock.java
@@ -1,6 +1,8 @@
 package org.getspout.spout.block;
 
+import org.spoutcraft.spoutcraftapi.World;
 import org.spoutcraft.spoutcraftapi.block.Block;
+import org.spoutcraft.spoutcraftapi.block.Chunk;
 
 public class SpoutcraftBlock implements Block{
 	public int getX() {
@@ -26,6 +28,60 @@ public class SpoutcraftBlock implements Block{
 	public void setHardness(float hardness) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public int getBlockPower() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public byte getLightLevel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTypeId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public World getWorld() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isLiquid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setData(byte data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean setTypeId(int type) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Chunk getChunk() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/minecraft/org/getspout/spout/block/SpoutcraftChunk.java
+++ src/minecraft/org/getspout/spout/block/SpoutcraftChunk.java
@@ -1,11 +1,73 @@
 package org.getspout.spout.block;
 
+import org.spoutcraft.spoutcraftapi.World;
 import org.spoutcraft.spoutcraftapi.block.Block;
 import org.spoutcraft.spoutcraftapi.block.Chunk;
+import org.spoutcraft.spoutcraftapi.entity.Entity;
 
 public class SpoutcraftChunk implements Chunk{
 
 	public Block getBlockAt(int x, int y, int z) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public World getWorld() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getZ() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isLoaded() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean load() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean load(boolean generate) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean unload() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean unload(boolean save) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean unload(boolean save, boolean safe) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Entity[] getEntities() {
 		// TODO Auto-generated method stub
 		return null;
 	}
