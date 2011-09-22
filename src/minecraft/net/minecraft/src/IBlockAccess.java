1eedfde935c8090a8dfa6f3f7aee644fcef82828
Afforess <Afforess@gmail.com>
2011-09-22 17:55:52 -0400

    Missed a file.

---
+++ src/minecraft/net/minecraft/src/IBlockAccess.java
@@ -0,0 +1,42 @@
+package net.minecraft.src;
+
+import net.minecraft.src.Material;
+import net.minecraft.src.TileEntity;
+import net.minecraft.src.WorldChunkManager;
+
+public interface IBlockAccess {
+
+	int getBlockId(int var1, int var2, int var3);
+
+	TileEntity getBlockTileEntity(int var1, int var2, int var3);
+
+	int func_35451_b(int var1, int var2, int var3, int var4);
+
+	float getBrightness(int var1, int var2, int var3, int var4);
+
+	float getLightBrightness(int var1, int var2, int var3);
+
+	int getBlockMetadata(int var1, int var2, int var3);
+
+	Material getBlockMaterial(int var1, int var2, int var3);
+
+	boolean isBlockOpaqueCube(int var1, int var2, int var3);
+
+	boolean isBlockNormalCube(int var1, int var2, int var3);
+
+	boolean isAirBlock(int var1, int var2, int var3);
+
+	WorldChunkManager getWorldChunkManager();
+
+	int func_35452_b();
+	
+	//Spout start
+	public int getGrassColorCache(int x, int y, int z);
+	
+	public void setGrassColorCache(int x, int y, int z, int color);
+	
+	public int getWaterColorCache(int x, int y, int z);
+	
+	public void setWaterColorCache(int x, int y, int z, int color);
+	//Spout end
+}
