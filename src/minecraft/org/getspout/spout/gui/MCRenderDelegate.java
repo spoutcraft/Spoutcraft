b5d45ed5225ba41cba7b6013d7180f8deacda8c9
Afforess <Afforess@gmail.com>
2011-09-24 01:06:42 -0400

    Renamed Texture packs and mods to Texture Packs Fixed type in Biome Colors hover over Updated about screen Cleanup

--- src/minecraft/org/getspout/spout/gui/MCRenderDelegate.java
+++ src/minecraft/org/getspout/spout/gui/MCRenderDelegate.java
@@ -2,7 +2,6 @@ package org.getspout.spout.gui;
 
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Random;
 import java.util.UUID;
 
 import net.minecraft.client.Minecraft;
@@ -326,17 +325,13 @@ public class MCRenderDelegate implements RenderDelegate {
 			GL11.glDisable(GL11.GL_DEPTH_TEST);
 			GL11.glDepthMask(false);
 			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-			GL11.glTranslatef((float) texture.getScreenX(), (float) texture.getScreenY(), 0); // moves
-																								// texture
-																								// into
-																								// place
+			GL11.glTranslatef((float) texture.getScreenX(), (float) texture.getScreenY(), 0); // moves texture into place
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureBinding.getTextureID());
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
 			Tessellator tessellator = Tessellator.instance;
 			tessellator.startDrawingQuads();
-			tessellator.addVertexWithUV(0.0D, texture.getHeight(), -90, 0.0D, 0.0D); // draw
-																						// corners
+			tessellator.addVertexWithUV(0.0D, texture.getHeight(), -90, 0.0D, 0.0D); // draw corners
 			tessellator.addVertexWithUV(texture.getWidth(), texture.getHeight(), -90, textureBinding.getWidth(), 0.0D);
 			tessellator.addVertexWithUV(texture.getWidth(), 0.0D, -90, textureBinding.getWidth(), textureBinding.getHeight());
 			tessellator.addVertexWithUV(0.0D, 0.0D, -90, 0.0D, textureBinding.getHeight());
@@ -440,8 +435,7 @@ public class MCRenderDelegate implements RenderDelegate {
 				int x = (int) bar.getScreenX() - icon * bar.getIconOffset();
 				int y = (int) bar.getScreenY();
 				
-				if(Minecraft.theMinecraft.thePlayer.func_35191_at().func_35760_d() <= 0.0F && bar.getUpdateCounter() % (foodLevel * 3 + 1) == 0)
-				{
+				if(Minecraft.theMinecraft.thePlayer.func_35191_at().func_35760_d() <= 0.0F && bar.getUpdateCounter() % (foodLevel * 3 + 1) == 0) {
 					y += GuiIngame.rand.nextInt(3) - 1;
 				}
 
@@ -458,22 +452,21 @@ public class MCRenderDelegate implements RenderDelegate {
 		}
 
 	}
-	
+
 	public void render(ExpBar bar) {
 		if (bar.isVisible()) {
-            int expCap = Minecraft.theMinecraft.thePlayer.xpBarCap();
-            if(expCap > 0)
-            {
-                char c = '\266';
-                int x = (int) bar.getScreenX();
-                int y = (int) bar.getScreenY();
-                int exp = (Minecraft.theMinecraft.thePlayer.currentXP * (c + 1)) / expCap;
-                RenderUtil.drawTexturedModalRectangle(x, y, 0, 64, c, 5, 0f);
-                if(exp > 0)
-                {
-                    RenderUtil.drawTexturedModalRectangle(x, y, 0, 69, exp, 5, 0f);
-                }
-            }
+			int expCap = Minecraft.theMinecraft.thePlayer.xpBarCap();
+			if(expCap > 0) {
+				char c = '\266';
+				int x = (int) bar.getScreenX();
+				int y = (int) bar.getScreenY();
+				int exp = (Minecraft.theMinecraft.thePlayer.currentXP * (c + 1)) / expCap;
+				RenderUtil.drawTexturedModalRectangle(x, y, 0, 64, c, 5, 0f);
+				if(exp > 0)
+				{
+					RenderUtil.drawTexturedModalRectangle(x, y, 0, 69, exp, 5, 0f);
+				}
+			}
 		}
 	}
 }
