75c6a51222467664cd72c30ab365371bfc74338e
Olloth <olloth@gmail.com>
2011-10-02 14:22:55 -0400

    Added clientside custom item support using Flint

---
+++ src/minecraft/org/getspout/spout/block/SpoutCustomItem.java
@@ -0,0 +1,12 @@
+package org.getspout.spout.block;
+
+import net.minecraft.src.Item;
+
+public class SpoutCustomItem extends Item {
+
+	public SpoutCustomItem(int i) {
+		super(i);
+		this.setHasSubtypes(true);
+	}
+
+}
