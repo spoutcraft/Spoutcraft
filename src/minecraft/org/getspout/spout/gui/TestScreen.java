09b0cf188e780110735d5526762683d9a062f650
narrowtux <narrow.m@gmail.com>
2011-10-10 20:02:16 +0200

    Implement a test screen. A button to summon the screen will be displayed in the Main Menu but only when started from the console. Feel free to play around in this class: org.getspout.spout.gui.TestScreen

---
+++ src/minecraft/org/getspout/spout/gui/TestScreen.java
@@ -0,0 +1,17 @@
+package org.getspout.spout.gui;
+
+import net.minecraft.src.GuiScreen;
+
+public class TestScreen extends GuiScreen {
+	
+	@Override
+	public void drawScreen(int var1, int var2, float var3) {
+		drawDefaultBackground();
+	}
+	
+	@Override
+	public void initGui() {
+		
+	}
+
+}
