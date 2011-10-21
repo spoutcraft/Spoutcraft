cded3bf2a8146b264a0de35934f5e39771a63408
Afforess <Afforess@gmail.com>
2011-10-20 20:05:24 -0400

    derp

---
+++ src/minecraft/org/getspout/spout/gui/settings/WaterBiomeColorsButton.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Spoutcraft (http://wiki.getspout.org/).
+ * 
+ * Spoutcraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Spoutcraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.getspout.spout.gui.settings;
+
+import net.minecraft.client.Minecraft;
+
+import org.getspout.spout.config.ConfigReader;
+import org.spoutcraft.spoutcraftapi.event.screen.ButtonClickEvent;
+import org.spoutcraft.spoutcraftapi.gui.GenericCheckBox;
+
+public class WaterBiomeColorsButton extends GenericCheckBox{
+	public WaterBiomeColorsButton() {
+		super("Water Biome Colors");
+		this.setChecked(ConfigReader.waterBiomeColors);
+		setTooltip("Water Biome Colors\nOn - water will have colors adjusted for biomes\nOff - vanilla water colors.");
+	}
+	
+	@Override
+	public void onButtonClick(ButtonClickEvent event) {
+		ConfigReader.waterBiomeColors = !ConfigReader.waterBiomeColors;
+		ConfigReader.write();
+		
+		if (Minecraft.theMinecraft.theWorld != null) {
+			Minecraft.theMinecraft.renderGlobal.updateAllRenderers();
+		}
+	}
+}
