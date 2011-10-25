5fc1260abaefef7d0f2c726303b835b53538d6a7
alta189 <stephendwilliams7@gmail.com>
2011-10-24 23:53:48 -0400

    New versioning

--- main/java/org/spoutcraft/spoutcraftapi/SpoutVersion.java
+++ main/java/org/spoutcraft/spoutcraftapi/SpoutVersion.java
@@ -17,31 +17,22 @@
 package org.spoutcraft.spoutcraftapi;
 
 public class SpoutVersion {
-	private final int major;
-	private final int minor;
 	private final int build;
-	private final int subbuild;
 
 	public SpoutVersion() {
-		major = 0;
-		minor = 0;
 		build = -1;
-		subbuild = 0;
 	}
 
-	public SpoutVersion(int major, int minor, int build, int subbuild) {
-		this.major = major;
-		this.minor = minor;
+	public SpoutVersion(int build) {
 		this.build = build;
-		this.subbuild = subbuild;
 	}
 
 	public long getVersion() {
-		return major * 100 + minor * 10 + build;
+		return build;
 	}
 
 	public String toString() {
-		return (new StringBuilder("")).append(major).append(".").append(minor).append(".").append(build).append(".").append(subbuild).toString();
+		return (new StringBuilder("")).append(build).toString();
 	}
 
 }
