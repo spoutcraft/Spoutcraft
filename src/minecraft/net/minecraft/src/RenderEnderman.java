f62a05dc3758c509e4f903b9430fd6717622d977
narrowtux <narrow.m@gmail.com>
2011-11-16 17:20:23 +0100

    License text and JAD crap

--- src/minecraft/net/minecraft/src/RenderEnderman.java
+++ src/minecraft/net/minecraft/src/RenderEnderman.java
@@ -1,7 +1,19 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
+/*
+ * This file is part of Spoutcraft (http://wiki.getspout.org/).
+ * 
+ * Spoutcraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Spoutcraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.minecraft.src;
 
 import java.util.Random;
@@ -10,12 +22,10 @@ import org.getspout.spout.entity.EntitySkinType;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 
-// Referenced classes of package net.minecraft.src:
-//            RenderLiving, ModelEnderman, EntityEnderman, Block, 
-//            RenderBlocks, EntityLiving, Entity
-
 public class RenderEnderman extends RenderLiving
 {
+    private ModelEnderman field_35444_c;
+    private Random field_35445_h;
 
     public RenderEnderman()
     {
@@ -112,7 +122,4 @@ public class RenderEnderman extends RenderLiving
     {
         func_35442_a((EntityEnderman)entity, d, d1, d2, f, f1);
     }
-
-    private ModelEnderman field_35444_c;
-    private Random field_35445_h;
 }
