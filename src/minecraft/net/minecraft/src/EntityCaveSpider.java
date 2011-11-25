b37b28dc4887b13bb1d932fba3963c06a8eb6cf5
alta189 <stephendwilliams7@gmail.com>
2011-11-25 04:53:32 -0500

    MC 1.0 EntityCaveSpider

--- src/minecraft/net/minecraft/src/EntityCaveSpider.java
+++ src/minecraft/net/minecraft/src/EntityCaveSpider.java
@@ -1,7 +1,6 @@
 package net.minecraft.src;
 
-import org.getspout.spout.entity.CraftCaveSpider;
-
+import org.getspout.spout.entity.CraftCaveSpider; // Spout
 import net.minecraft.src.Entity;
 import net.minecraft.src.EntityLiving;
 import net.minecraft.src.EntitySpider;
@@ -11,39 +10,43 @@ import net.minecraft.src.World;
 
 public class EntityCaveSpider extends EntitySpider {
 
-	public EntityCaveSpider(World var1) {
-		super(var1);
-		this.texture = "/mob/cavespider.png";
-		this.setSize(0.7F, 0.5F);
+   public EntityCaveSpider(World var1) {
+      super(var1);
+      this.texture = "/mob/cavespider.png";
+      this.setSize(0.7F, 0.5F);
 		//Spout start
 		this.spoutEntity = new CraftCaveSpider(this);
 		//Spout end
-	}
+   }
 
-	public float func_35188_k_() {
-		return 0.7F;
-	}
+   public int getMaxHealth() {
+      return 12;
+   }
 
-	protected boolean attackEntityAsMob(Entity var1) {
-		if(super.attackEntityAsMob(var1)) {
-			if(var1 instanceof EntityLiving) {
-				byte var2 = 0;
-				if(this.worldObj.difficultySetting > 1) {
-					if(this.worldObj.difficultySetting == 2) {
-						var2 = 7;
-					} else if(this.worldObj.difficultySetting == 3) {
-						var2 = 15;
-					}
-				}
+   public float spiderScaleAmount() {
+      return 0.7F;
+   }
 
-				if(var2 > 0) {
-					((EntityLiving)var1).func_35165_a(new PotionEffect(Potion.potionPoison.id, var2 * 20, 0));
-				}
-			}
+   protected boolean attackEntityAsMob(Entity var1) {
+      if(super.attackEntityAsMob(var1)) {
+         if(var1 instanceof EntityLiving) {
+            byte var2 = 0;
+            if(this.worldObj.difficultySetting > 1) {
+               if(this.worldObj.difficultySetting == 2) {
+                  var2 = 7;
+               } else if(this.worldObj.difficultySetting == 3) {
+                  var2 = 15;
+               }
+            }
 
-			return true;
-		} else {
-			return false;
-		}
-	}
+            if(var2 > 0) {
+               ((EntityLiving)var1).addPotionEffect(new PotionEffect(Potion.potionPoison.id, var2 * 20, 0));
+            }
+         }
+
+         return true;
+      } else {
+         return false;
+      }
+   }
 }
