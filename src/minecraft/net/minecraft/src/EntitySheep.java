61391ee6cae922139dc5514f436fae791aaf0dfd
alta189 <stephendwilliams7@gmail.com>
2011-11-25 04:57:06 -0500

    MC 1.0 EntitySheep

--- src/minecraft/net/minecraft/src/EntitySheep.java
+++ src/minecraft/net/minecraft/src/EntitySheep.java
@@ -1,9 +1,7 @@
 package net.minecraft.src;
 
 import java.util.Random;
-
-import org.getspout.spout.entity.CraftSheep;
-
+import org.getspout.spout.entity.CraftSheep; // Spout
 import net.minecraft.src.Block;
 import net.minecraft.src.EntityAnimal;
 import net.minecraft.src.EntityItem;
@@ -15,106 +13,122 @@ import net.minecraft.src.World;
 
 public class EntitySheep extends EntityAnimal {
 
-	public static final float[][] fleeceColorTable = new float[][]{{1.0F, 1.0F, 1.0F}, {0.95F, 0.7F, 0.2F}, {0.9F, 0.5F, 0.85F}, {0.6F, 0.7F, 0.95F}, {0.9F, 0.9F, 0.2F}, {0.5F, 0.8F, 0.1F}, {0.95F, 0.7F, 0.8F}, {0.3F, 0.3F, 0.3F}, {0.6F, 0.6F, 0.6F}, {0.3F, 0.6F, 0.7F}, {0.7F, 0.4F, 0.9F}, {0.2F, 0.4F, 0.8F}, {0.5F, 0.4F, 0.3F}, {0.4F, 0.5F, 0.2F}, {0.8F, 0.3F, 0.3F}, {0.1F, 0.1F, 0.1F}};
+   public static final float[][] fleeceColorTable = new float[][]{{1.0F, 1.0F, 1.0F}, {0.95F, 0.7F, 0.2F}, {0.9F, 0.5F, 0.85F}, {0.6F, 0.7F, 0.95F}, {0.9F, 0.9F, 0.2F}, {0.5F, 0.8F, 0.1F}, {0.95F, 0.7F, 0.8F}, {0.3F, 0.3F, 0.3F}, {0.6F, 0.6F, 0.6F}, {0.3F, 0.6F, 0.7F}, {0.7F, 0.4F, 0.9F}, {0.2F, 0.4F, 0.8F}, {0.5F, 0.4F, 0.3F}, {0.4F, 0.5F, 0.2F}, {0.8F, 0.3F, 0.3F}, {0.1F, 0.1F, 0.1F}};
 
 
-	public EntitySheep(World var1) {
-		super(var1);
-		this.texture = "/mob/sheep.png";
-		this.setSize(0.9F, 1.3F);
+   public EntitySheep(World var1) {
+      super(var1);
+      this.texture = "/mob/sheep.png";
+      this.setSize(0.9F, 1.3F);
 		
 		//Spout start
 		this.spoutEntity = new CraftSheep(this);
 		//Spout end
-	}
+   }
 
-	protected void entityInit() {
-		super.entityInit();
-		this.dataWatcher.addObject(16, new Byte((byte)0));
-	}
+   public int getMaxHealth() {
+      return 8;
+   }
 
-	protected void dropFewItems(boolean var1) {
-		if(!this.getSheared()) {
-			this.entityDropItem(new ItemStack(Block.cloth.blockID, 1, this.getFleeceColor()), 0.0F);
-		}
+   protected void entityInit() {
+      super.entityInit();
+      this.dataWatcher.addObject(16, new Byte((byte)0));
+   }
 
-	}
+   protected void dropFewItems(boolean var1, int var2) {
+      if(!this.getSheared()) {
+         this.entityDropItem(new ItemStack(Block.cloth.blockID, 1, this.getFleeceColor()), 0.0F);
+      }
 
-	protected int getDropItemId() {
-		return Block.cloth.blockID;
-	}
+   }
 
-	public boolean interact(EntityPlayer var1) {
-		ItemStack var2 = var1.inventory.getCurrentItem();
-		if(var2 != null && var2.itemID == Item.shears.shiftedIndex && !this.getSheared()) {
-			if(!this.worldObj.multiplayerWorld) {
-				this.setSheared(true);
-				int var3 = 2 + this.rand.nextInt(3);
+   protected int getDropItemId() {
+      return Block.cloth.blockID;
+   }
 
-				for(int var4 = 0; var4 < var3; ++var4) {
-					EntityItem var5 = this.entityDropItem(new ItemStack(Block.cloth.blockID, 1, this.getFleeceColor()), 1.0F);
-					var5.motionY += (double)(this.rand.nextFloat() * 0.05F);
-					var5.motionX += (double)((this.rand.nextFloat() - this.rand.nextFloat()) * 0.1F);
-					var5.motionZ += (double)((this.rand.nextFloat() - this.rand.nextFloat()) * 0.1F);
-				}
-			}
+   public boolean interact(EntityPlayer var1) {
+      ItemStack var2 = var1.inventory.getCurrentItem();
+      if(var2 != null && var2.itemID == Item.shears.shiftedIndex && !this.getSheared()) {
+         if(!this.worldObj.multiplayerWorld) {
+            this.setSheared(true);
+            int var3 = 2 + this.rand.nextInt(3);
 
-			var2.damageItem(1, var1);
-		}
+            for(int var4 = 0; var4 < var3; ++var4) {
+               EntityItem var5 = this.entityDropItem(new ItemStack(Block.cloth.blockID, 1, this.getFleeceColor()), 1.0F);
+               var5.motionY += (double)(this.rand.nextFloat() * 0.05F);
+               var5.motionX += (double)((this.rand.nextFloat() - this.rand.nextFloat()) * 0.1F);
+               var5.motionZ += (double)((this.rand.nextFloat() - this.rand.nextFloat()) * 0.1F);
+            }
+         }
 
-		return false;
-	}
+         var2.damageItem(1, var1);
+      }
 
-	public void writeEntityToNBT(NBTTagCompound var1) {
-		super.writeEntityToNBT(var1);
-		var1.setBoolean("Sheared", this.getSheared());
-		var1.setByte("Color", (byte)this.getFleeceColor());
-	}
+      return super.interact(var1);
+   }
 
-	public void readEntityFromNBT(NBTTagCompound var1) {
-		super.readEntityFromNBT(var1);
-		this.setSheared(var1.getBoolean("Sheared"));
-		this.setFleeceColor(var1.getByte("Color"));
-	}
+   public void writeEntityToNBT(NBTTagCompound var1) {
+      super.writeEntityToNBT(var1);
+      var1.setBoolean("Sheared", this.getSheared());
+      var1.setByte("Color", (byte)this.getFleeceColor());
+   }
 
-	protected String getLivingSound() {
-		return "mob.sheep";
-	}
+   public void readEntityFromNBT(NBTTagCompound var1) {
+      super.readEntityFromNBT(var1);
+      this.setSheared(var1.getBoolean("Sheared"));
+      this.setFleeceColor(var1.getByte("Color"));
+   }
 
-	protected String getHurtSound() {
-		return "mob.sheep";
-	}
+   protected String getLivingSound() {
+      return "mob.sheep";
+   }
 
-	protected String getDeathSound() {
-		return "mob.sheep";
-	}
+   protected String getHurtSound() {
+      return "mob.sheep";
+   }
 
-	public int getFleeceColor() {
-		return this.dataWatcher.getWatchableObjectByte(16) & 15;
-	}
+   protected String getDeathSound() {
+      return "mob.sheep";
+   }
 
-	public void setFleeceColor(int var1) {
-		byte var2 = this.dataWatcher.getWatchableObjectByte(16);
-		this.dataWatcher.updateObject(16, Byte.valueOf((byte)(var2 & 240 | var1 & 15)));
-	}
+   public int getFleeceColor() {
+      return this.dataWatcher.getWatchableObjectByte(16) & 15;
+   }
 
-	public boolean getSheared() {
-		return (this.dataWatcher.getWatchableObjectByte(16) & 16) != 0;
-	}
+   public void setFleeceColor(int var1) {
+      byte var2 = this.dataWatcher.getWatchableObjectByte(16);
+      this.dataWatcher.updateObject(16, Byte.valueOf((byte)(var2 & 240 | var1 & 15)));
+   }
 
-	public void setSheared(boolean var1) {
-		byte var2 = this.dataWatcher.getWatchableObjectByte(16);
-		if(var1) {
-			this.dataWatcher.updateObject(16, Byte.valueOf((byte)(var2 | 16)));
-		} else {
-			this.dataWatcher.updateObject(16, Byte.valueOf((byte)(var2 & -17)));
-		}
+   public boolean getSheared() {
+      return (this.dataWatcher.getWatchableObjectByte(16) & 16) != 0;
+   }
 
-	}
+   public void setSheared(boolean var1) {
+      byte var2 = this.dataWatcher.getWatchableObjectByte(16);
+      if(var1) {
+         this.dataWatcher.updateObject(16, Byte.valueOf((byte)(var2 | 16)));
+      } else {
+         this.dataWatcher.updateObject(16, Byte.valueOf((byte)(var2 & -17)));
+      }
 
-	public static int getRandomFleeceColor(Random var0) {
-		int var1 = var0.nextInt(100);
-		return var1 < 5?15:(var1 < 10?7:(var1 < 15?8:(var1 < 18?12:(var0.nextInt(500) == 0?6:0))));
-	}
+   }
+
+   public static int getRandomFleeceColor(Random var0) {
+      int var1 = var0.nextInt(100);
+      return var1 < 5?15:(var1 < 10?7:(var1 < 15?8:(var1 < 18?12:(var0.nextInt(500) == 0?6:0))));
+   }
+
+   protected EntityAnimal func_40145_a(EntityAnimal var1) {
+      EntitySheep var2 = (EntitySheep)var1;
+      EntitySheep var3 = new EntitySheep(this.worldObj);
+      if(this.rand.nextBoolean()) {
+         var3.setFleeceColor(this.getFleeceColor());
+      } else {
+         var3.setFleeceColor(var2.getFleeceColor());
+      }
+
+      return var3;
+   }
 
 }
