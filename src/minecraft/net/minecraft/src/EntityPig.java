c81dab6e213646a5524e5987f8d6454eb9426c09
alta189 <stephendwilliams7@gmail.com>
2011-11-25 04:56:28 -0500

    MC 1.0 EntityPig

--- src/minecraft/net/minecraft/src/EntityPig.java
+++ src/minecraft/net/minecraft/src/EntityPig.java
@@ -1,7 +1,6 @@
 package net.minecraft.src;
 
-import org.getspout.spout.entity.CraftPig;
-
+import org.getspout.spout.entity.CraftPig; // Spout
 import net.minecraft.src.AchievementList;
 import net.minecraft.src.EntityAnimal;
 import net.minecraft.src.EntityLightningBolt;
@@ -13,81 +12,92 @@ import net.minecraft.src.World;
 
 public class EntityPig extends EntityAnimal {
 
-	public EntityPig(World var1) {
-		super(var1);
-		this.texture = "/mob/pig.png";
-		this.setSize(0.9F, 0.9F);
+   public EntityPig(World var1) {
+      super(var1);
+      this.texture = "/mob/pig.png";
+      this.setSize(0.9F, 0.9F);
 		//Spout start
 		this.spoutEntity = new CraftPig(this);
 		//Spout end
-	}
+   }
 
-	protected void entityInit() {
-		this.dataWatcher.addObject(16, Byte.valueOf((byte)0));
-	}
+   public int getMaxHealth() {
+      return 10;
+   }
 
-	public void writeEntityToNBT(NBTTagCompound var1) {
-		super.writeEntityToNBT(var1);
-		var1.setBoolean("Saddle", this.getSaddled());
-	}
+   protected void entityInit() {
+      super.entityInit();
+      this.dataWatcher.addObject(16, Byte.valueOf((byte)0));
+   }
 
-	public void readEntityFromNBT(NBTTagCompound var1) {
-		super.readEntityFromNBT(var1);
-		this.setSaddled(var1.getBoolean("Saddle"));
-	}
+   public void writeEntityToNBT(NBTTagCompound var1) {
+      super.writeEntityToNBT(var1);
+      var1.setBoolean("Saddle", this.getSaddled());
+   }
 
-	protected String getLivingSound() {
-		return "mob.pig";
-	}
+   public void readEntityFromNBT(NBTTagCompound var1) {
+      super.readEntityFromNBT(var1);
+      this.setSaddled(var1.getBoolean("Saddle"));
+   }
 
-	protected String getHurtSound() {
-		return "mob.pig";
-	}
+   protected String getLivingSound() {
+      return "mob.pig";
+   }
 
-	protected String getDeathSound() {
-		return "mob.pigdeath";
-	}
+   protected String getHurtSound() {
+      return "mob.pig";
+   }
 
-	public boolean interact(EntityPlayer var1) {
-		if(this.getSaddled() && !this.worldObj.multiplayerWorld && (this.riddenByEntity == null || this.riddenByEntity == var1)) {
-			var1.mountEntity(this);
-			return true;
-		} else {
-			return false;
-		}
-	}
+   protected String getDeathSound() {
+      return "mob.pigdeath";
+   }
 
-	protected int getDropItemId() {
-		return this.fire > 0?Item.porkCooked.shiftedIndex:Item.porkRaw.shiftedIndex;
-	}
+   public boolean interact(EntityPlayer var1) {
+      if(super.interact(var1)) {
+         return true;
+      } else if(this.getSaddled() && !this.worldObj.multiplayerWorld && (this.riddenByEntity == null || this.riddenByEntity == var1)) {
+         var1.mountEntity(this);
+         return true;
+      } else {
+         return false;
+      }
+   }
 
-	public boolean getSaddled() {
-		return (this.dataWatcher.getWatchableObjectByte(16) & 1) != 0;
-	}
+   protected int getDropItemId() {
+      return this.isBurning()?Item.porkCooked.shiftedIndex:Item.porkRaw.shiftedIndex;
+   }
 
-	public void setSaddled(boolean var1) {
-		if(var1) {
-			this.dataWatcher.updateObject(16, Byte.valueOf((byte)1));
-		} else {
-			this.dataWatcher.updateObject(16, Byte.valueOf((byte)0));
-		}
+   public boolean getSaddled() {
+      return (this.dataWatcher.getWatchableObjectByte(16) & 1) != 0;
+   }
 
-	}
+   public void setSaddled(boolean var1) {
+      if(var1) {
+         this.dataWatcher.updateObject(16, Byte.valueOf((byte)1));
+      } else {
+         this.dataWatcher.updateObject(16, Byte.valueOf((byte)0));
+      }
 
-	public void onStruckByLightning(EntityLightningBolt var1) {
-		if(!this.worldObj.multiplayerWorld) {
-			EntityPigZombie var2 = new EntityPigZombie(this.worldObj);
-			var2.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, this.rotationPitch);
-			this.worldObj.entityJoinedWorld(var2);
-			this.setEntityDead();
-		}
-	}
+   }
 
-	protected void fall(float var1) {
-		super.fall(var1);
-		if(var1 > 5.0F && this.riddenByEntity instanceof EntityPlayer) {
-			((EntityPlayer)this.riddenByEntity).triggerAchievement(AchievementList.flyPig);
-		}
+   public void onStruckByLightning(EntityLightningBolt var1) {
+      if(!this.worldObj.multiplayerWorld) {
+         EntityPigZombie var2 = new EntityPigZombie(this.worldObj);
+         var2.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, this.rotationPitch);
+         this.worldObj.entityJoinedWorld(var2);
+         this.setEntityDead();
+      }
+   }
 
-	}
+   protected void fall(float var1) {
+      super.fall(var1);
+      if(var1 > 5.0F && this.riddenByEntity instanceof EntityPlayer) {
+         ((EntityPlayer)this.riddenByEntity).triggerAchievement(AchievementList.flyPig);
+      }
+
+   }
+
+   protected EntityAnimal func_40145_a(EntityAnimal var1) {
+      return new EntityPig(this.worldObj);
+   }
 }
