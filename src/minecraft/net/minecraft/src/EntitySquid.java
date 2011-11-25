54e8cc7a43aec937334d955d18a6bea41af48fed
alta189 <stephendwilliams7@gmail.com>
2011-11-25 04:57:53 -0500

    MC 1.0 EntitySquid

--- src/minecraft/net/minecraft/src/EntitySquid.java
+++ src/minecraft/net/minecraft/src/EntitySquid.java
@@ -1,6 +1,6 @@
 package net.minecraft.src;
 
-import org.getspout.spout.entity.CraftSquid;
+import org.getspout.spout.entity.CraftSquid; // Spout
 
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.EntityWaterMob;
@@ -13,145 +13,156 @@ import net.minecraft.src.World;
 
 public class EntitySquid extends EntityWaterMob {
 
-	public float field_21089_a = 0.0F;
-	public float field_21088_b = 0.0F;
-	public float field_21087_c = 0.0F;
-	public float field_21086_f = 0.0F;
-	public float field_21085_g = 0.0F;
-	public float field_21084_h = 0.0F;
-	public float field_21083_i = 0.0F;
-	public float field_21082_j = 0.0F;
-	private float randomMotionSpeed = 0.0F;
-	private float field_21080_l = 0.0F;
-	private float field_21079_m = 0.0F;
-	private float randomMotionVecX = 0.0F;
-	private float randomMotionVecY = 0.0F;
-	private float randomMotionVecZ = 0.0F;
+   public float field_21089_a = 0.0F;
+   public float field_21088_b = 0.0F;
+   public float field_21087_c = 0.0F;
+   public float field_21086_f = 0.0F;
+   public float field_21085_g = 0.0F;
+   public float field_21084_h = 0.0F;
+   public float field_21083_i = 0.0F;
+   public float field_21082_j = 0.0F;
+   private float randomMotionSpeed = 0.0F;
+   private float field_21080_l = 0.0F;
+   private float field_21079_m = 0.0F;
+   private float randomMotionVecX = 0.0F;
+   private float randomMotionVecY = 0.0F;
+   private float randomMotionVecZ = 0.0F;
 
 
-	public EntitySquid(World var1) {
-		super(var1);
-		this.texture = "/mob/squid.png";
-		this.setSize(0.95F, 0.95F);
-		this.field_21080_l = 1.0F / (this.rand.nextFloat() + 1.0F) * 0.2F;
+   public EntitySquid(World var1) {
+      super(var1);
+      this.texture = "/mob/squid.png";
+      this.setSize(0.95F, 0.95F);
+      this.field_21080_l = 1.0F / (this.rand.nextFloat() + 1.0F) * 0.2F;
 		//Spout start
 		this.spoutEntity = new CraftSquid(this);
 		//Spout end
-	}
+   }
 
-	public void writeEntityToNBT(NBTTagCompound var1) {
-		super.writeEntityToNBT(var1);
-	}
+   public int getMaxHealth() {
+      return 10;
+   }
 
-	public void readEntityFromNBT(NBTTagCompound var1) {
-		super.readEntityFromNBT(var1);
-	}
+   public void writeEntityToNBT(NBTTagCompound var1) {
+      super.writeEntityToNBT(var1);
+   }
 
-	protected String getLivingSound() {
-		return null;
-	}
+   public void readEntityFromNBT(NBTTagCompound var1) {
+      super.readEntityFromNBT(var1);
+   }
 
-	protected String getHurtSound() {
-		return null;
-	}
+   protected String getLivingSound() {
+      return null;
+   }
 
-	protected String getDeathSound() {
-		return null;
-	}
+   protected String getHurtSound() {
+      return null;
+   }
 
-	protected float getSoundVolume() {
-		return 0.4F;
-	}
+   protected String getDeathSound() {
+      return null;
+   }
 
-	protected int getDropItemId() {
-		return 0;
-	}
+   protected float getSoundVolume() {
+      return 0.4F;
+   }
 
-	protected void dropFewItems(boolean var1) {
-		int var2 = this.rand.nextInt(3) + 1;
+   protected int getDropItemId() {
+      return 0;
+   }
 
-		for(int var3 = 0; var3 < var2; ++var3) {
-			this.entityDropItem(new ItemStack(Item.dyePowder, 1, 0), 0.0F);
-		}
+   protected void dropFewItems(boolean var1, int var2) {
+      int var3 = this.rand.nextInt(3 + var2) + 1;
 
-	}
+      for(int var4 = 0; var4 < var3; ++var4) {
+         this.entityDropItem(new ItemStack(Item.dyePowder, 1, 0), 0.0F);
+      }
 
-	public boolean interact(EntityPlayer var1) {
-		return false;
-	}
+   }
 
-	public boolean isInWater() {
-		return this.worldObj.handleMaterialAcceleration(this.boundingBox.expand(0.0D, -0.6000000238418579D, 0.0D), Material.water, this);
-	}
+   public boolean interact(EntityPlayer var1) {
+      return super.interact(var1);
+   }
 
-	public void onLivingUpdate() {
-		super.onLivingUpdate();
-		this.field_21088_b = this.field_21089_a;
-		this.field_21086_f = this.field_21087_c;
-		this.field_21084_h = this.field_21085_g;
-		this.field_21082_j = this.field_21083_i;
-		this.field_21085_g += this.field_21080_l;
-		if(this.field_21085_g > 6.2831855F) {
-			this.field_21085_g -= 6.2831855F;
-			if(this.rand.nextInt(10) == 0) {
-				this.field_21080_l = 1.0F / (this.rand.nextFloat() + 1.0F) * 0.2F;
-			}
-		}
+   public boolean isInWater() {
+      return this.worldObj.handleMaterialAcceleration(this.boundingBox.expand(0.0D, -0.6000000238418579D, 0.0D), Material.water, this);
+   }
 
-		if(this.isInWater()) {
-			float var1;
-			if(this.field_21085_g < 3.1415927F) {
-				var1 = this.field_21085_g / 3.1415927F;
-				this.field_21083_i = MathHelper.sin(var1 * var1 * 3.1415927F) * 3.1415927F * 0.25F;
-				if((double)var1 > 0.75D) {
-					this.randomMotionSpeed = 1.0F;
-					this.field_21079_m = 1.0F;
-				} else {
-					this.field_21079_m *= 0.8F;
-				}
-			} else {
-				this.field_21083_i = 0.0F;
-				this.randomMotionSpeed *= 0.9F;
-				this.field_21079_m *= 0.99F;
-			}
+   public void onLivingUpdate() {
+      super.onLivingUpdate();
+      this.field_21088_b = this.field_21089_a;
+      this.field_21086_f = this.field_21087_c;
+      this.field_21084_h = this.field_21085_g;
+      this.field_21082_j = this.field_21083_i;
+      this.field_21085_g += this.field_21080_l;
+      if(this.field_21085_g > 6.2831855F) {
+         this.field_21085_g -= 6.2831855F;
+         if(this.rand.nextInt(10) == 0) {
+            this.field_21080_l = 1.0F / (this.rand.nextFloat() + 1.0F) * 0.2F;
+         }
+      }
 
-			if(!this.isMultiplayerEntity) {
-				this.motionX = (double)(this.randomMotionVecX * this.randomMotionSpeed);
-				this.motionY = (double)(this.randomMotionVecY * this.randomMotionSpeed);
-				this.motionZ = (double)(this.randomMotionVecZ * this.randomMotionSpeed);
-			}
+      if(this.isInWater()) {
+         float var1;
+         if(this.field_21085_g < 3.1415927F) {
+            var1 = this.field_21085_g / 3.1415927F;
+            this.field_21083_i = MathHelper.sin(var1 * var1 * 3.1415927F) * 3.1415927F * 0.25F;
+            if((double)var1 > 0.75D) {
+               this.randomMotionSpeed = 1.0F;
+               this.field_21079_m = 1.0F;
+            } else {
+               this.field_21079_m *= 0.8F;
+            }
+         } else {
+            this.field_21083_i = 0.0F;
+            this.randomMotionSpeed *= 0.9F;
+            this.field_21079_m *= 0.99F;
+         }
 
-			var1 = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
-			this.renderYawOffset += (-((float)Math.atan2(this.motionX, this.motionZ)) * 180.0F / 3.1415927F - this.renderYawOffset) * 0.1F;
-			this.rotationYaw = this.renderYawOffset;
-			this.field_21087_c += 3.1415927F * this.field_21079_m * 1.5F;
-			this.field_21089_a += (-((float)Math.atan2((double)var1, this.motionY)) * 180.0F / 3.1415927F - this.field_21089_a) * 0.1F;
-		} else {
-			this.field_21083_i = MathHelper.abs(MathHelper.sin(this.field_21085_g)) * 3.1415927F * 0.25F;
-			if(!this.isMultiplayerEntity) {
-				this.motionX = 0.0D;
-				this.motionY -= 0.08D;
-				this.motionY *= 0.9800000190734863D;
-				this.motionZ = 0.0D;
-			}
+         if(!this.isMultiplayerEntity) {
+            this.motionX = (double)(this.randomMotionVecX * this.randomMotionSpeed);
+            this.motionY = (double)(this.randomMotionVecY * this.randomMotionSpeed);
+            this.motionZ = (double)(this.randomMotionVecZ * this.randomMotionSpeed);
+         }
 
-			this.field_21089_a = (float)((double)this.field_21089_a + (double)(-90.0F - this.field_21089_a) * 0.02D);
-		}
+         var1 = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
+         this.renderYawOffset += (-((float)Math.atan2(this.motionX, this.motionZ)) * 180.0F / 3.1415927F - this.renderYawOffset) * 0.1F;
+         this.rotationYaw = this.renderYawOffset;
+         this.field_21087_c += 3.1415927F * this.field_21079_m * 1.5F;
+         this.field_21089_a += (-((float)Math.atan2((double)var1, this.motionY)) * 180.0F / 3.1415927F - this.field_21089_a) * 0.1F;
+      } else {
+         this.field_21083_i = MathHelper.abs(MathHelper.sin(this.field_21085_g)) * 3.1415927F * 0.25F;
+         if(!this.isMultiplayerEntity) {
+            this.motionX = 0.0D;
+            this.motionY -= 0.08D;
+            this.motionY *= 0.9800000190734863D;
+            this.motionZ = 0.0D;
+         }
 
-	}
+         this.field_21089_a = (float)((double)this.field_21089_a + (double)(-90.0F - this.field_21089_a) * 0.02D);
+      }
 
-	public void moveEntityWithHeading(float var1, float var2) {
-		this.moveEntity(this.motionX, this.motionY, this.motionZ);
-	}
+   }
 
-	protected void updateEntityActionState() {
-		if(this.rand.nextInt(50) == 0 || !this.inWater || this.randomMotionVecX == 0.0F && this.randomMotionVecY == 0.0F && this.randomMotionVecZ == 0.0F) {
-			float var1 = this.rand.nextFloat() * 3.1415927F * 2.0F;
-			this.randomMotionVecX = MathHelper.cos(var1) * 0.2F;
-			this.randomMotionVecY = -0.1F + this.rand.nextFloat() * 0.2F;
-			this.randomMotionVecZ = MathHelper.sin(var1) * 0.2F;
-		}
+   public void moveEntityWithHeading(float var1, float var2) {
+      this.moveEntity(this.motionX, this.motionY, this.motionZ);
+   }
 
-		this.despawnEntity();
-	}
+   protected void updateEntityActionState() {
+      ++this.entityAge;
+      if(this.entityAge > 100) {
+         this.randomMotionVecX = this.randomMotionVecY = this.randomMotionVecZ = 0.0F;
+      } else if(this.rand.nextInt(50) == 0 || !this.inWater || this.randomMotionVecX == 0.0F && this.randomMotionVecY == 0.0F && this.randomMotionVecZ == 0.0F) {
+         float var1 = this.rand.nextFloat() * 3.1415927F * 2.0F;
+         this.randomMotionVecX = MathHelper.cos(var1) * 0.2F;
+         this.randomMotionVecY = -0.1F + this.rand.nextFloat() * 0.2F;
+         this.randomMotionVecZ = MathHelper.sin(var1) * 0.2F;
+      }
+
+      this.despawnEntity();
+   }
+
+   public boolean getCanSpawnHere() {
+      return this.posY > 45.0D && this.posY < (double)this.worldObj.field_35470_e && super.getCanSpawnHere();
+   }
 }
