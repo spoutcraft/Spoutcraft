24b961e6a2792227bf1cf1a5e535a63e159b0c2f
alta189 <stephendwilliams7@gmail.com>
2011-11-25 04:58:34 -0500

    MC 1.0 EntityZombie

--- src/minecraft/net/minecraft/src/EntityZombie.java
+++ src/minecraft/net/minecraft/src/EntityZombie.java
@@ -1,49 +1,60 @@
 package net.minecraft.src;
 
-import org.getspout.spout.entity.CraftZombie;
-
+import org.getspout.spout.entity.CraftZombie; // Spout
 import net.minecraft.src.EntityMob;
+import net.minecraft.src.EnumCreatureAttribute;
 import net.minecraft.src.Item;
 import net.minecraft.src.MathHelper;
 import net.minecraft.src.World;
 
 public class EntityZombie extends EntityMob {
 
-	public EntityZombie(World var1) {
-		super(var1);
-		this.texture = "/mob/zombie.png";
-		this.moveSpeed = 0.5F;
-		this.attackStrength = 5;
-		
+   public EntityZombie(World var1) {
+      super(var1);
+      this.texture = "/mob/zombie.png";
+      this.moveSpeed = 0.5F;
+      this.attackStrength = 4;
+   }
+
+   public int getMaxHealth() {
+      return 20;
 		//Spout start
 		this.spoutEntity = new CraftZombie(this);
 		//Spout end
-	}
+   }
 
-	public void onLivingUpdate() {
-		if(this.worldObj.isDaytime() && !this.worldObj.multiplayerWorld) {
-			float var1 = this.getEntityBrightness(1.0F);
-			if(var1 > 0.5F && this.worldObj.canBlockSeeTheSky(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ)) && this.rand.nextFloat() * 30.0F < (var1 - 0.4F) * 2.0F) {
-				this.fire = 300;
-			}
-		}
+   protected int func_40119_ar() {
+      return 2;
+   }
 
-		super.onLivingUpdate();
-	}
+   public void onLivingUpdate() {
+      if(this.worldObj.isDaytime() && !this.worldObj.multiplayerWorld) {
+         float var1 = this.getEntityBrightness(1.0F);
+         if(var1 > 0.5F && this.worldObj.canBlockSeeTheSky(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ)) && this.rand.nextFloat() * 30.0F < (var1 - 0.4F) * 2.0F) {
+            this.func_40046_d(8);
+         }
+      }
 
-	protected String getLivingSound() {
-		return "mob.zombie";
-	}
+      super.onLivingUpdate();
+   }
 
-	protected String getHurtSound() {
-		return "mob.zombiehurt";
-	}
+   protected String getLivingSound() {
+      return "mob.zombie";
+   }
 
-	protected String getDeathSound() {
-		return "mob.zombiedeath";
-	}
+   protected String getHurtSound() {
+      return "mob.zombiehurt";
+   }
 
-	protected int getDropItemId() {
-		return Item.rottenFlesh.shiftedIndex;
-	}
+   protected String getDeathSound() {
+      return "mob.zombiedeath";
+   }
+
+   protected int getDropItemId() {
+      return Item.rottenFlesh.shiftedIndex;
+   }
+
+   public EnumCreatureAttribute func_40124_t() {
+      return EnumCreatureAttribute.UNDEAD;
+   }
 }
