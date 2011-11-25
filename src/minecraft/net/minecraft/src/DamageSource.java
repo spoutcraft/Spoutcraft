99109e6395d6e9fbc30ffd63c26e15b3801b9c46
alta189 <stephendwilliams7@gmail.com>
2011-11-25 06:26:28 -0500

    MC 1.0 Damage Source

--- src/minecraft/net/minecraft/src/DamageSource.java
+++ src/minecraft/net/minecraft/src/DamageSource.java
@@ -10,22 +10,24 @@ import net.minecraft.src.EntityPlayer;
 
 public class DamageSource {
 
-	public static DamageSource inFire = new DamageSource("inFire");
-	public static DamageSource onFire = (new DamageSource("onFire")).func_35528_f();
-	public static DamageSource lava = new DamageSource("lava");
-	public static DamageSource inWall = (new DamageSource("inWall")).func_35528_f();
-	public static DamageSource drown = (new DamageSource("drown")).func_35528_f();
-	public static DamageSource starve = (new DamageSource("starve")).func_35528_f();
+	public static DamageSource inFire = (new DamageSource("inFire")).func_40546_j();
+	public static DamageSource onFire = (new DamageSource("onFire")).setDamageBypassesArmor().func_40546_j();
+	public static DamageSource lava = (new DamageSource("lava")).func_40546_j();
+	public static DamageSource inWall = (new DamageSource("inWall")).setDamageBypassesArmor();
+	public static DamageSource drown = (new DamageSource("drown")).setDamageBypassesArmor();
+	public static DamageSource starve = (new DamageSource("starve")).setDamageBypassesArmor();
 	public static DamageSource cactus = new DamageSource("cactus");
-	public static DamageSource fall = new DamageSource("fall");
-	public static DamageSource outOfWorld = (new DamageSource("outOfWorld")).func_35528_f().func_35531_g();
-	public static DamageSource generic = (new DamageSource("generic")).func_35528_f();
+	public static DamageSource fall = (new DamageSource("fall")).setDamageBypassesArmor();
+	public static DamageSource outOfWorld = (new DamageSource("outOfWorld")).setDamageBypassesArmor().setDamageAllowedInCreativeMode();
+	public static DamageSource generic = (new DamageSource("generic")).setDamageBypassesArmor();
 	public static DamageSource explosion = new DamageSource("explosion");
-	public static DamageSource magic = (new DamageSource("magic")).func_35528_f();
-	private boolean field_35543_n = false;
-	private boolean field_35544_o = false;
-	private float field_35551_p = 0.3F;
-	public String field_35546_m;
+	public static DamageSource magic = (new DamageSource("magic")).setDamageBypassesArmor();
+	private boolean isBlockable = false;
+	private boolean isDamageAllowedInCreativeMode = false;
+	private float hungerDamage = 0.3F;
+	private boolean field_40549_q;
+	private boolean field_40548_r;
+	public String damageType;
 
 
 	public static DamageSource causeMobDamage(EntityLiving var0) {
@@ -37,34 +39,47 @@ public class DamageSource {
 	}
 
 	public static DamageSource causeArrowDamage(EntityArrow var0, Entity var1) {
-		return new EntityDamageSourceIndirect("arrow", var0, var1);
+		return (new EntityDamageSourceIndirect("arrow", var0, var1)).func_40544_c();
 	}
 
 	public static DamageSource causeFireballDamage(EntityFireball var0, Entity var1) {
-		return new EntityDamageSourceIndirect("fireball", var0, var1);
+		return (new EntityDamageSourceIndirect("fireball", var0, var1)).func_40546_j().func_40544_c();
 	}
 
 	public static DamageSource causeThrownDamage(Entity var0, Entity var1) {
-		return new EntityDamageSourceIndirect("thrown", var0, var1);
+		return (new EntityDamageSourceIndirect("thrown", var0, var1)).func_40544_c();
 	}
 
-	public boolean func_35534_b() {
-		return this.field_35543_n;
+	public static DamageSource func_40542_b(Entity var0, Entity var1) {
+		return (new EntityDamageSourceIndirect("indirectMagic", var0, var1)).setDamageBypassesArmor();
 	}
 
-	public float func_35533_c() {
-		return this.field_35551_p;
+	public boolean func_40547_b() {
+		return this.field_40548_r;
 	}
 
-	public boolean func_35529_d() {
-		return this.field_35544_o;
+	public DamageSource func_40544_c() {
+		this.field_40548_r = true;
+		return this;
+	}
+
+	public boolean unblockable() {
+		return this.isBlockable;
+	}
+
+	public float getHungerDamage() {
+		return this.hungerDamage;
+	}
+
+	public boolean canHarmInCreative() {
+		return this.isDamageAllowedInCreativeMode;
 	}
 
 	protected DamageSource(String var1) {
-		this.field_35546_m = var1;
+		this.damageType = var1;
 	}
 
-	public Entity func_35526_e() {
+	public Entity getSourceOfDamage() {
 		return this.getEntity();
 	}
 
@@ -72,15 +87,28 @@ public class DamageSource {
 		return null;
 	}
 
-	private DamageSource func_35528_f() {
-		this.field_35543_n = true;
-		this.field_35551_p = 0.0F;
+	protected DamageSource setDamageBypassesArmor() {
+		this.isBlockable = true;
+		this.hungerDamage = 0.0F;
 		return this;
 	}
 
-	private DamageSource func_35531_g() {
-		this.field_35544_o = true;
+	protected DamageSource setDamageAllowedInCreativeMode() {
+		this.isDamageAllowedInCreativeMode = true;
 		return this;
+	}
+
+	protected DamageSource func_40546_j() {
+		this.field_40549_q = true;
+		return this;
+	}
+
+	public boolean func_40543_k() {
+		return this.field_40549_q;
+	}
+
+	public String func_40545_l() {
+		return this.damageType;
 	}
 
 }
