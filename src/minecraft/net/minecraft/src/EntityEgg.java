d140d263c461c3ce96c04381768eb0b498a01da7
alta189 <stephendwilliams7@gmail.com>
2011-11-25 04:54:25 -0500

    MC 1.0 EntityEgg

--- src/minecraft/net/minecraft/src/EntityEgg.java
+++ src/minecraft/net/minecraft/src/EntityEgg.java
@@ -1,269 +1,62 @@
 package net.minecraft.src;
 
-import java.util.List;
-
-import org.getspout.spout.entity.CraftEgg;
-
-import net.minecraft.src.AxisAlignedBB;
+import org.getspout.spout.entity.CraftEgg; // Spout
 import net.minecraft.src.DamageSource;
-import net.minecraft.src.Entity;
 import net.minecraft.src.EntityChicken;
 import net.minecraft.src.EntityLiving;
-import net.minecraft.src.EntityPlayer;
-import net.minecraft.src.Item;
-import net.minecraft.src.ItemStack;
-import net.minecraft.src.MathHelper;
+import net.minecraft.src.EntityThrowable;
 import net.minecraft.src.MovingObjectPosition;
-import net.minecraft.src.NBTTagCompound;
-import net.minecraft.src.Vec3D;
 import net.minecraft.src.World;
 
-public class EntityEgg extends Entity {
+public class EntityEgg extends EntityThrowable {
 
-	private int xTile = -1;
-	private int yTile = -1;
-	private int zTile = -1;
-	private int inTile = 0;
-	private boolean inGround = false;
-	public int shake = 0;
-	public EntityLiving field_20051_g; //Spout private->public
-	private int field_20050_h;
-	private int field_20049_i = 0;
-
-
-	public EntityEgg(World var1) {
-		super(var1);
-		this.setSize(0.25F, 0.25F);
+   public EntityEgg(World var1) {
+      super(var1);
 		//Spout start
 		this.spoutEntity = new CraftEgg(this);
 		//Spout end
-	}
+   }
 
-	protected void entityInit() {}
+   public EntityEgg(World var1, EntityLiving var2) {
+      super(var1, var2);
+		//Spout start
+		this.spoutEntity = new CraftEgg(this);
+		//Spout end
+   }
 
-	public boolean isInRangeToRenderDist(double var1) {
-		double var3 = this.boundingBox.getAverageEdgeLength() * 4.0D;
-		var3 *= 64.0D;
-		return var1 < var3 * var3;
-	}
+   public EntityEgg(World var1, double var2, double var4, double var6) {
+      super(var1, var2, var4, var6);
+		//Spout start
+		this.spoutEntity = new CraftEgg(this);
+		//Spout end
+   }
 
-	public EntityEgg(World var1, EntityLiving var2) {
-		super(var1);
-		this.field_20051_g = var2;
-		this.setSize(0.25F, 0.25F);
-		this.setLocationAndAngles(var2.posX, var2.posY + (double)var2.getEyeHeight(), var2.posZ, var2.rotationYaw, var2.rotationPitch);
-		this.posX -= (double)(MathHelper.cos(this.rotationYaw / 180.0F * 3.1415927F) * 0.16F);
-		this.posY -= 0.10000000149011612D;
-		this.posZ -= (double)(MathHelper.sin(this.rotationYaw / 180.0F * 3.1415927F) * 0.16F);
-		this.setPosition(this.posX, this.posY, this.posZ);
-		this.yOffset = 0.0F;
-		float var3 = 0.4F;
-		this.motionX = (double)(-MathHelper.sin(this.rotationYaw / 180.0F * 3.1415927F) * MathHelper.cos(this.rotationPitch / 180.0F * 3.1415927F) * var3);
-		this.motionZ = (double)(MathHelper.cos(this.rotationYaw / 180.0F * 3.1415927F) * MathHelper.cos(this.rotationPitch / 180.0F * 3.1415927F) * var3);
-		this.motionY = (double)(-MathHelper.sin(this.rotationPitch / 180.0F * 3.1415927F) * var3);
-		this.setEggHeading(this.motionX, this.motionY, this.motionZ, 1.5F, 1.0F);
-	}
+   protected void func_40078_a(MovingObjectPosition var1) {
+      if(var1.entityHit != null && var1.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.field_40083_c), 0)) {
+         ;
+      }
 
-	public EntityEgg(World var1, double var2, double var4, double var6) {
-		super(var1);
-		this.field_20050_h = 0;
-		this.setSize(0.25F, 0.25F);
-		this.setPosition(var2, var4, var6);
-		this.yOffset = 0.0F;
-	}
+      if(!this.worldObj.multiplayerWorld && this.rand.nextInt(8) == 0) {
+         byte var2 = 1;
+         if(this.rand.nextInt(32) == 0) {
+            var2 = 4;
+         }
 
-	public void setEggHeading(double var1, double var3, double var5, float var7, float var8) {
-		float var9 = MathHelper.sqrt_double(var1 * var1 + var3 * var3 + var5 * var5);
-		var1 /= (double)var9;
-		var3 /= (double)var9;
-		var5 /= (double)var9;
-		var1 += this.rand.nextGaussian() * 0.007499999832361937D * (double)var8;
-		var3 += this.rand.nextGaussian() * 0.007499999832361937D * (double)var8;
-		var5 += this.rand.nextGaussian() * 0.007499999832361937D * (double)var8;
-		var1 *= (double)var7;
-		var3 *= (double)var7;
-		var5 *= (double)var7;
-		this.motionX = var1;
-		this.motionY = var3;
-		this.motionZ = var5;
-		float var10 = MathHelper.sqrt_double(var1 * var1 + var5 * var5);
-		this.prevRotationYaw = this.rotationYaw = (float)(Math.atan2(var1, var5) * 180.0D / 3.1415927410125732D);
-		this.prevRotationPitch = this.rotationPitch = (float)(Math.atan2(var3, (double)var10) * 180.0D / 3.1415927410125732D);
-		this.field_20050_h = 0;
-	}
+         for(int var3 = 0; var3 < var2; ++var3) {
+            EntityChicken var4 = new EntityChicken(this.worldObj);
+            var4.func_40142_a_(-24000);
+            var4.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
+            this.worldObj.entityJoinedWorld(var4);
+         }
+      }
 
-	public void setVelocity(double var1, double var3, double var5) {
-		this.motionX = var1;
-		this.motionY = var3;
-		this.motionZ = var5;
-		if(this.prevRotationPitch == 0.0F && this.prevRotationYaw == 0.0F) {
-			float var7 = MathHelper.sqrt_double(var1 * var1 + var5 * var5);
-			this.prevRotationYaw = this.rotationYaw = (float)(Math.atan2(var1, var5) * 180.0D / 3.1415927410125732D);
-			this.prevRotationPitch = this.rotationPitch = (float)(Math.atan2(var3, (double)var7) * 180.0D / 3.1415927410125732D);
-		}
+      for(int var5 = 0; var5 < 8; ++var5) {
+         this.worldObj.spawnParticle("snowballpoof", this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
+      }
 
-	}
+      if(!this.worldObj.multiplayerWorld) {
+         this.setEntityDead();
+      }
 
-	public void onUpdate() {
-		this.lastTickPosX = this.posX;
-		this.lastTickPosY = this.posY;
-		this.lastTickPosZ = this.posZ;
-		super.onUpdate();
-		if(this.shake > 0) {
-			--this.shake;
-		}
-
-		if(this.inGround) {
-			int var1 = this.worldObj.getBlockId(this.xTile, this.yTile, this.zTile);
-			if(var1 == this.inTile) {
-				++this.field_20050_h;
-				if(this.field_20050_h == 1200) {
-					this.setEntityDead();
-				}
-
-				return;
-			}
-
-			this.inGround = false;
-			this.motionX *= (double)(this.rand.nextFloat() * 0.2F);
-			this.motionY *= (double)(this.rand.nextFloat() * 0.2F);
-			this.motionZ *= (double)(this.rand.nextFloat() * 0.2F);
-			this.field_20050_h = 0;
-			this.field_20049_i = 0;
-		} else {
-			++this.field_20049_i;
-		}
-
-		Vec3D var15 = Vec3D.createVector(this.posX, this.posY, this.posZ);
-		Vec3D var2 = Vec3D.createVector(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
-		MovingObjectPosition var3 = this.worldObj.rayTraceBlocks(var15, var2);
-		var15 = Vec3D.createVector(this.posX, this.posY, this.posZ);
-		var2 = Vec3D.createVector(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
-		if(var3 != null) {
-			var2 = Vec3D.createVector(var3.hitVec.xCoord, var3.hitVec.yCoord, var3.hitVec.zCoord);
-		}
-
-		if(!this.worldObj.multiplayerWorld) {
-			Entity var4 = null;
-			List var5 = this.worldObj.getEntitiesWithinAABBExcludingEntity(this, this.boundingBox.addCoord(this.motionX, this.motionY, this.motionZ).expand(1.0D, 1.0D, 1.0D));
-			double var6 = 0.0D;
-
-			for(int var8 = 0; var8 < var5.size(); ++var8) {
-				Entity var9 = (Entity)var5.get(var8);
-				if(var9.canBeCollidedWith() && (var9 != this.field_20051_g || this.field_20049_i >= 5)) {
-					float var10 = 0.3F;
-					AxisAlignedBB var11 = var9.boundingBox.expand((double)var10, (double)var10, (double)var10);
-					MovingObjectPosition var12 = var11.func_1169_a(var15, var2);
-					if(var12 != null) {
-						double var13 = var15.distanceTo(var12.hitVec);
-						if(var13 < var6 || var6 == 0.0D) {
-							var4 = var9;
-							var6 = var13;
-						}
-					}
-				}
-			}
-
-			if(var4 != null) {
-				var3 = new MovingObjectPosition(var4);
-			}
-		}
-
-		if(var3 != null) {
-			if(var3.entityHit != null && var3.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.field_20051_g), 0)) {
-				;
-			}
-
-			if(!this.worldObj.multiplayerWorld && this.rand.nextInt(8) == 0) {
-				byte var16 = 1;
-				if(this.rand.nextInt(32) == 0) {
-					var16 = 4;
-				}
-
-				for(int var17 = 0; var17 < var16; ++var17) {
-					EntityChicken var20 = new EntityChicken(this.worldObj);
-					var20.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
-					this.worldObj.entityJoinedWorld(var20);
-				}
-			}
-
-			for(int var19 = 0; var19 < 8; ++var19) {
-				this.worldObj.spawnParticle("snowballpoof", this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
-			}
-
-			this.setEntityDead();
-		}
-
-		this.posX += this.motionX;
-		this.posY += this.motionY;
-		this.posZ += this.motionZ;
-		float var21 = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
-		this.rotationYaw = (float)(Math.atan2(this.motionX, this.motionZ) * 180.0D / 3.1415927410125732D);
-
-		for(this.rotationPitch = (float)(Math.atan2(this.motionY, (double)var21) * 180.0D / 3.1415927410125732D); this.rotationPitch - this.prevRotationPitch < -180.0F; this.prevRotationPitch -= 360.0F) {
-			;
-		}
-
-		while(this.rotationPitch - this.prevRotationPitch >= 180.0F) {
-			this.prevRotationPitch += 360.0F;
-		}
-
-		while(this.rotationYaw - this.prevRotationYaw < -180.0F) {
-			this.prevRotationYaw -= 360.0F;
-		}
-
-		while(this.rotationYaw - this.prevRotationYaw >= 180.0F) {
-			this.prevRotationYaw += 360.0F;
-		}
-
-		this.rotationPitch = this.prevRotationPitch + (this.rotationPitch - this.prevRotationPitch) * 0.2F;
-		this.rotationYaw = this.prevRotationYaw + (this.rotationYaw - this.prevRotationYaw) * 0.2F;
-		float var18 = 0.99F;
-		float var22 = 0.03F;
-		if(this.isInWater()) {
-			for(int var7 = 0; var7 < 4; ++var7) {
-				float var23 = 0.25F;
-				this.worldObj.spawnParticle("bubble", this.posX - this.motionX * (double)var23, this.posY - this.motionY * (double)var23, this.posZ - this.motionZ * (double)var23, this.motionX, this.motionY, this.motionZ);
-			}
-
-			var18 = 0.8F;
-		}
-
-		this.motionX *= (double)var18;
-		this.motionY *= (double)var18;
-		this.motionZ *= (double)var18;
-		this.motionY -= (double)var22;
-		this.setPosition(this.posX, this.posY, this.posZ);
-	}
-
-	public void writeEntityToNBT(NBTTagCompound var1) {
-		var1.setShort("xTile", (short)this.xTile);
-		var1.setShort("yTile", (short)this.yTile);
-		var1.setShort("zTile", (short)this.zTile);
-		var1.setByte("inTile", (byte)this.inTile);
-		var1.setByte("shake", (byte)this.shake);
-		var1.setByte("inGround", (byte)(this.inGround?1:0));
-	}
-
-	public void readEntityFromNBT(NBTTagCompound var1) {
-		this.xTile = var1.getShort("xTile");
-		this.yTile = var1.getShort("yTile");
-		this.zTile = var1.getShort("zTile");
-		this.inTile = var1.getByte("inTile") & 255;
-		this.shake = var1.getByte("shake") & 255;
-		this.inGround = var1.getByte("inGround") == 1;
-	}
-
-	public void onCollideWithPlayer(EntityPlayer var1) {
-		if(this.inGround && this.field_20051_g == var1 && this.shake <= 0 && var1.inventory.addItemStackToInventory(new ItemStack(Item.arrow, 1))) {
-			this.worldObj.playSoundAtEntity(this, "random.pop", 0.2F, ((this.rand.nextFloat() - this.rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
-			var1.onItemPickup(this, 1);
-			this.setEntityDead();
-		}
-
-	}
-
-	public float getShadowSize() {
-		return 0.0F;
-	}
+   }
 }
