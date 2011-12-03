6b0872490e80070e999c2499cba08d5dde0b2f97
Olloth <olloth@gmail.com>
2011-12-03 15:44:32 -0500

    Updated clear method to include new special TileEntities

--- src/minecraft/net/minecraft/src/TileEntityRenderer.java
+++ src/minecraft/net/minecraft/src/TileEntityRenderer.java
@@ -139,6 +139,10 @@ public class TileEntityRenderer {
 		this.specialRendererMap.put(TileEntitySign.class, new TileEntitySignRenderer());
 		this.specialRendererMap.put(TileEntityMobSpawner.class, new TileEntityMobSpawnerRenderer());
 		this.specialRendererMap.put(TileEntityPiston.class, new TileEntityRendererPiston());
+		this.specialRendererMap.put(TileEntityChest.class, new TileEntityChestRenderer());
+		this.specialRendererMap.put(TileEntityEnchantmentTable.class, new RenderEnchantmentTable());
+		this.specialRendererMap.put(TileEntityEndPortal.class, new RenderEndPortal());
+		
 		Iterator var1 = this.specialRendererMap.values().iterator();
 
 		while (var1.hasNext()) {
