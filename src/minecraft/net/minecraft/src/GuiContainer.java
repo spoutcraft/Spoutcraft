005923b2d8f9e60e5aa98523e838b213d12d769a
Afforess <afforess@gmail.com>
2011-12-14 10:37:26 -0500

    fix potion tooltip (special case... :(  )

--- src/minecraft/net/minecraft/src/GuiContainer.java
+++ src/minecraft/net/minecraft/src/GuiContainer.java
@@ -102,7 +102,7 @@ public abstract class GuiContainer extends GuiScreen
 			List<String> list = itemstack.func_40712_q();
 			org.spoutcraft.spoutcraftapi.material.Material item = MaterialData.getMaterial(slot.getStack().itemID, (short)(slot.getStack().getItemDamage()));
 			String custom = item != null ? item.getName() : null;
-			if (custom != null) {
+			if (custom != null && slot.getStack().itemID != MaterialData.potion.getRawId()) {
 				list.set(0, custom);
 			}
 			//Spout End
