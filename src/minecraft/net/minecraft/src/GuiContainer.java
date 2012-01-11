6ff8b4beebf1841a7ffb394308475c8febc0af96
Afforess <afforess@gmail.com>
2012-01-11 16:18:17 -0500

    more formatting.

--- src/minecraft/net/minecraft/src/GuiContainer.java
+++ src/minecraft/net/minecraft/src/GuiContainer.java
@@ -101,7 +101,7 @@ public abstract class GuiContainer extends GuiScreen
 			//Spout Start
 			List<String> list = itemstack.func_40712_q();
 			org.spoutcraft.spoutcraftapi.material.Material item = MaterialData.getMaterial(slot.getStack().itemID, (short)(slot.getStack().getItemDamage()));
-			String custom = item != null ? item.getName() : null;
+			String custom = item != null ? String.format(item.getName(), String.valueOf(slot.getStack().getItemDamage())) : null;
 			if (custom != null && slot.getStack().itemID != MaterialData.potion.getRawId()) {
 				list.set(0, custom);
 			}
