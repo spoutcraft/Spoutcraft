5d32fe1d66dcf5028002f7ed24ced4172f2cf447
Top_Cat <webmaster@thomasc.co.uk>
2012-01-25 20:05:07 +0000

    Might need this

---
+++ main/java/org/spoutcraft/spoutcraftapi/material/item/SpawnEgg.java
@@ -0,0 +1,9 @@
+package org.spoutcraft.spoutcraftapi.material.item;
+
+public class SpawnEgg extends GenericItem{
+
+	public SpawnEgg(String name, int id, int data) {
+		super(name, id, data);
+	}
+
+}
