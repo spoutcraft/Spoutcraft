87ead59037b882e9e870cccb182725d0b4f8a7a2
Jonathan Duncan <JonathanDuncan@gmail.com>
2012-03-17 14:35:09 +0000

    Enabled custom names on Potions

--- src/minecraft/net/minecraft/src/GuiContainer.java
+++ src/minecraft/net/minecraft/src/GuiContainer.java
@@ -115,7 +115,7 @@ public abstract class GuiContainer extends GuiScreen
 			List<String> list = itemstack.getItemNameandInformation();
 			org.spoutcraft.spoutcraftapi.material.Material item = MaterialData.getMaterial(slot.getStack().itemID, (short)(slot.getStack().getItemDamage()));
 			String custom = item != null ? String.format(item.getName(), String.valueOf(slot.getStack().getItemDamage())) : null;
-			if (custom != null && slot.getStack().itemID != MaterialData.potion.getRawId()) {
+			if (custom != null) {
 				list.set(0, custom);
 			}
 			if(list.size() > 0)
