46d11101b7f3f7449dd6db83ebd29e6221637d12
narrowtux <narrow.m@gmail.com>
2012-03-26 18:41:57 +0200

    Fix player labels not rendering behind blocks (and also that they don't get a background)

--- src/minecraft/net/minecraft/src/RenderLiving.java
+++ src/minecraft/net/minecraft/src/RenderLiving.java
@@ -284,6 +284,7 @@ public class RenderLiving extends Render {
 			GL11.glDisable(GL11.GL_LIGHTING);
 			GL11.glDepthMask(false);
 			GL11.glDisable(GL11.GL_DEPTH_TEST);
+			GL11.glDisable(GL11.GL_ALPHA_TEST);
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			Tessellator var14 = Tessellator.instance;
