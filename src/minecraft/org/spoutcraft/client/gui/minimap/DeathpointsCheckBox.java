a531e9e5f5123736710c4aabb7942ace089902fa
Afforess <Afforess@gmail.com>
2012-03-29 20:13:28 -0400

    fix backwards equality check

--- src/minecraft/org/spoutcraft/client/gui/minimap/DeathpointsCheckBox.java
+++ src/minecraft/org/spoutcraft/client/gui/minimap/DeathpointsCheckBox.java
@@ -6,12 +6,12 @@ import org.spoutcraft.spoutcraftapi.gui.GenericCheckBox;
 public class DeathpointsCheckBox extends GenericCheckBox{
 	public DeathpointsCheckBox() {
 		super("Deathpoints");
-		setChecked(!MinimapConfig.getInstance().isDeathpoints());
+		setChecked(MinimapConfig.getInstance().isDeathpoints());
 		setTooltip("Deathpoints\nAdd waypoints to the map every time you die, with\na timestamp of when you died. Deathpoints can\n be removed and altered like regular waypoints.");
 	}
 
 	@Override
 	public void onButtonClick(ButtonClickEvent event) {
-		MinimapConfig.getInstance().setDeathpoints(!isChecked());
+		MinimapConfig.getInstance().setDeathpoints(isChecked());
 	}
 }
