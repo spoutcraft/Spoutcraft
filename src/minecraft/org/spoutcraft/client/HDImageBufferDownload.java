4f899d8e3ea90323ebee8af63512f9b8729b1c15
Afforess <Afforess@gmail.com>
2012-04-20 18:51:50 -0400

    strip alpha transparency from skins

--- src/minecraft/org/spoutcraft/client/HDImageBufferDownload.java
+++ src/minecraft/org/spoutcraft/client/HDImageBufferDownload.java
@@ -10,23 +10,32 @@ public class HDImageBufferDownload implements ImageBuffer {
 	private int imageWidth;
 	private int imageHeight;
 
-	public BufferedImage parseUserSkin(BufferedImage par1BufferedImage) {
-		if (par1BufferedImage == null) {
+	public BufferedImage parseUserSkin(BufferedImage image) {
+		if (image == null) {
 			return null;
 		}
 
-		imageWidth = par1BufferedImage.getWidth(null);
-		imageHeight = imageWidth / 2;
-		BufferedImage bufferedimage1 = new BufferedImage(imageWidth, imageHeight, 2);
-		Graphics g = bufferedimage1.getGraphics();
-		g.drawImage(par1BufferedImage, 0, 0, null);
-		g.dispose();
-		imageData = ((DataBufferInt)bufferedimage1.getRaster().getDataBuffer()).getData();
-		func_884_b(0, 0, imageWidth / 2, imageHeight / 2);
-		func_885_a(imageWidth / 2, 0, imageWidth, imageHeight);
-		func_884_b(0, imageHeight / 2, imageWidth, imageHeight);
+		try {
+			imageWidth = Math.max(64, image.getWidth(null));
+			imageHeight = imageWidth / 2;
+			BufferedImage bufferedimage1 = new BufferedImage(imageWidth, imageHeight, 2);
+			Graphics g = bufferedimage1.getGraphics();
+			g.drawImage(image, 0, 0, null);
+			g.dispose();
+			imageData = ((DataBufferInt)bufferedimage1.getRaster().getDataBuffer()).getData();
+			func_884_b(0, 0, imageWidth / 2, imageHeight / 2);
+			func_885_a(imageWidth / 2, 0, imageWidth, imageHeight);
+			func_884_b(0, imageHeight / 2, imageWidth, imageHeight);
+			
+			for (int i = 0; i < imageData.length; i++) {
+				imageData[i] &= 0xFFFFFF;
+			}
+		}
+		catch (Exception e) {
+			return null;
+		}
 
-		return par1BufferedImage;
+		return image;
 	}
 
 	private void func_885_a(int par1, int par2, int par3, int par4) {
