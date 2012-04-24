f8794594ea5e2e7e4b4837dfdb9b471b5baea08c
Pamelloes <joshuabrot@gmail.com>
2012-04-23 21:47:49 -0500

    Updated for new screen types.

--- src/minecraft/org/spoutcraft/client/gui/ScreenUtil.java
+++ src/minecraft/org/spoutcraft/client/gui/ScreenUtil.java
@@ -17,22 +17,36 @@
 package org.spoutcraft.client.gui;
 
 import net.minecraft.src.GuiAchievements;
+import net.minecraft.src.GuiBrewingStand;
 import net.minecraft.src.GuiChat;
 import net.minecraft.src.GuiChest;
+import net.minecraft.src.GuiContainerCreative;
 import net.minecraft.src.GuiCrafting;
 import net.minecraft.src.GuiDispenser;
 import net.minecraft.src.GuiEditSign;
+import net.minecraft.src.GuiEnchantment;
 import net.minecraft.src.GuiFurnace;
 import net.minecraft.src.GuiGameOver;
 import net.minecraft.src.GuiIngameMenu;
 import net.minecraft.src.GuiInventory;
+import net.minecraft.src.GuiLanguage;
 import net.minecraft.src.GuiScreen;
 import net.minecraft.src.GuiSleepMP;
 import net.minecraft.src.GuiStats;
+import net.minecraft.src.GuiWinGame;
 import net.minecraft.src.StatFileWriter;
 
 import org.spoutcraft.client.SpoutClient;
+import org.spoutcraft.client.controls.GuiAmbigousInput;
+import org.spoutcraft.client.gui.chat.GuiChatSettings;
+import org.spoutcraft.client.gui.chat.GuiListEdit;
+import org.spoutcraft.client.gui.chat.GuiURLConfirm;
 import org.spoutcraft.client.gui.controls.GuiControls;
+import org.spoutcraft.client.gui.controls.GuiEditShortcut;
+import org.spoutcraft.client.gui.minimap.GuiAddWaypoint;
+import org.spoutcraft.client.gui.minimap.GuiMinimapMenu;
+import org.spoutcraft.client.gui.minimap.GuiMoveMinimap;
+import org.spoutcraft.client.gui.minimap.GuiOverviewMap;
 import org.spoutcraft.client.gui.settings.GameSettingsScreen;
 import org.spoutcraft.spoutcraftapi.gui.*;
 
@@ -72,6 +86,24 @@ public class ScreenUtil {
 		case GAME_OVER_SCREEN:
 			toOpen = new GuiGameOver();
 			break;
+		case CHANGE_LANGUAGE:
+			toOpen = new GuiLanguage(new GameSettingsScreen(new GuiIngameMenu()), SpoutClient.getHandle().gameSettings);
+			break;
+		case MINIMAP_SETTINGS:
+			toOpen = new GuiMinimapMenu(new GameSettingsScreen(new GuiIngameMenu()));
+			break;
+		case CHAT_SETTINGS:
+			toOpen = new GuiChatSettings(new GameSettingsScreen(new GuiIngameMenu()));
+			break;
+		case MOVE_MINIMAP:
+			toOpen = new GuiMoveMinimap(new GuiMinimapMenu(new GameSettingsScreen(new GuiIngameMenu())));
+			break;
+		case OVERVIEW_MAP:
+			toOpen = new GuiOverviewMap();
+			break;
+		case WIN_GAME:
+			toOpen = new GuiWinGame();
+			break;
 		}
 		SpoutClient.getHandle().displayGuiScreen(toOpen);
 	}
@@ -81,39 +113,66 @@ public class ScreenUtil {
 		if (gui == null) {
 			screen = ScreenType.GAME_SCREEN;
 		}
-		if (gui instanceof GuiChat) {
-			screen = ScreenType.CHAT_SCREEN;
-		} else if (gui instanceof GuiSleepMP) {
-			screen = ScreenType.SLEEP_SCREEN;
-		} else if (gui instanceof CustomScreen) {
+		if (gui instanceof CustomScreen) {
 			screen = ScreenType.CUSTOM_SCREEN;
-		} else if (gui instanceof GuiInventory) {
-			screen = ScreenType.PLAYER_INVENTORY;
-		} else if (gui instanceof GuiChest) {
-			screen = ScreenType.CHEST_INVENTORY;
-		} else if (gui instanceof GuiDispenser) {
-			screen = ScreenType.DISPENSER_INVENTORY;
-		} else if (gui instanceof GuiFurnace) {
-			screen = ScreenType.FURNACE_INVENTORY;
-		} else if (gui instanceof GuiIngameMenu) {
-			screen = ScreenType.INGAME_MENU;
-		} else if (gui instanceof GameSettingsScreen) {
-			screen = ScreenType.OPTIONS_MENU;
 		} else if (gui instanceof GameSettingsScreen) {
 			screen = ScreenType.VIDEO_SETTINGS_MENU;
-		} else if (gui instanceof GuiControls) {
-			screen = ScreenType.CONTROLS_MENU;
 		} else if (gui instanceof GuiAchievements) {
 			screen = ScreenType.ACHIEVEMENTS_SCREEN;
+		} else if (gui instanceof GuiAddWaypoint) {
+			screen = ScreenType.ADD_WAYPOINT;
+		} else if (gui instanceof GuiSleepMP) {
+			screen = ScreenType.SLEEP_SCREEN;
+		} else if (gui instanceof GuiChat) {
+			screen = ScreenType.CHAT_SCREEN;
+		} else if (gui instanceof GuiBrewingStand) {
+			screen = ScreenType.BREWING_STAND_INVENTORY;
+		} else if (gui instanceof GuiChest) {
+			screen = ScreenType.CHEST_INVENTORY;
+		} else if (gui instanceof GuiContainerCreative) {
+			screen = ScreenType.PLAYER_INVENTORY_CREATIVE;
 		} else if (gui instanceof GuiCrafting) {
 			screen = ScreenType.WORKBENCH_INVENTORY;
-		} else if (gui instanceof GuiGameOver) {
-			screen = ScreenType.GAME_OVER_SCREEN;
+		} else if (gui instanceof GuiDispenser) {
+			screen = ScreenType.DISPENSER_INVENTORY;
+		} else if (gui instanceof GuiEnchantment) {
+			screen = ScreenType.ENCHANTMENT_INVENTORY;
+		} else if (gui instanceof GuiFurnace) {
+			screen = ScreenType.FURNACE_INVENTORY;
+		} else if (gui instanceof GuiInventory) {
+			screen = ScreenType.PLAYER_INVENTORY;
+		} else if (gui instanceof GuiEditShortcut) {
+			screen = ScreenType.EDIT_SHORTCUT;
 		} else if (gui instanceof GuiEditSign) {
 			screen = ScreenType.SIGN_SCREEN;
+		} else if (gui instanceof GuiGameOver) {
+			screen = ScreenType.GAME_OVER_SCREEN;
+		} else if (gui instanceof GuiIngameMenu) {
+			screen = ScreenType.INGAME_MENU;
+		} else if (gui instanceof GuiLanguage) {
+			screen = ScreenType.CHANGE_LANGUAGE;
+		} else if (gui instanceof GuiMinimapMenu) {
+			screen = ScreenType.MINIMAP_SETTINGS;
+		} else if (gui instanceof GuiAmbigousInput) {
+			screen = ScreenType.AMBIGUOUS_SHORTCUT;
+		} else if (gui instanceof GuiChatSettings) {
+			screen = ScreenType.CHAT_SETTINGS;
+		} else if (gui instanceof GuiControls) {
+			screen = ScreenType.CONTROLS_MENU;
+		} else if (gui instanceof GuiListEdit) {
+			screen = ScreenType.LIST_EDIT;
+		} else if (gui instanceof GuiMoveMinimap) {
+			screen = ScreenType.MOVE_MINIMAP;
+		} else if (gui instanceof GuiOverviewMap) {
+			screen = ScreenType.OVERVIEW_MAP;
 		} else if (gui instanceof GuiStats) {
 			screen = ScreenType.STATISTICS_SCREEN;
+		} else if (gui instanceof GuiWinGame) {
+			screen = ScreenType.WIN_GAME;
+		} else if (gui instanceof GuiURLConfirm) {
+			screen = ScreenType.CONFIRM_URL;
 		}
+		
 		return screen;
 	}
 }
