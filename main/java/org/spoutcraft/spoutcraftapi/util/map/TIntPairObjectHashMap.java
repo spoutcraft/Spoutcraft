597fa5dc669c4395e99ffcd1010aa90f57c61583
Afforess <Afforess@gmail.com>
2012-04-26 16:45:52 -0400

    Remove Commons dep

---
+++ main/java/org/spoutcraft/spoutcraftapi/util/map/TIntPairObjectHashMap.java
@@ -0,0 +1,96 @@
+/*
+ * This file is part of SpoutCommons (http://www.spout.org/).
+ *
+ * SpoutCommons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SpoutCommons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.spoutcraft.spoutcraftapi.util.map;
+
+import java.util.Collection;
+
+import gnu.trove.iterator.TLongObjectIterator;
+import gnu.trove.map.hash.TLongObjectHashMap;
+import gnu.trove.set.TLongSet;
+
+/**
+ * A simplistic map that supports a pair of integers for keys, using a trove long object hashmap in the backend.
+ * @author Afforess
+ *
+ */
+public class TIntPairObjectHashMap<K>{
+	private TLongObjectHashMap<K> map;
+
+	public TIntPairObjectHashMap() {
+		map = new TLongObjectHashMap<K>(100);
+	}
+
+	public TIntPairObjectHashMap(int capacity) {
+		map = new TLongObjectHashMap<K>(capacity);
+	}
+
+	public K put(int key1, int key2, K value) {
+		long key = (((long)key1)<<32) | (((long)key2) & 0xFFFFFFFFL);
+		return map.put(key, value);
+	}
+
+	public K get(int key1, int key2) {
+		long key = (((long)key1)<<32) | (((long)key2) & 0xFFFFFFFFL);
+		return map.get(key);
+	}
+
+	public boolean containsKey(int key1, int key2) {
+		long key = (((long)key1)<<32) | (((long)key2) & 0xFFFFFFFFL);
+		return map.containsKey(key);
+	}
+
+	public void clear() {
+		map.clear();
+	}
+
+	public boolean containsValue(K val) {
+		return map.containsValue(val);
+	}
+
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	public TLongObjectIterator<K> iterator() {
+		return map.iterator();
+	}
+
+	public TLongSet keySet() {
+		return map.keySet();
+	}
+
+	public long[] keys() {
+		return map.keys();
+	}
+
+	public K remove(int key1, int key2) {
+		long key = (((long)key1)<<32) | (((long)key2) & 0xFFFFFFFFL);
+		return map.remove(key);
+	}
+
+	public int size() {
+		return map.size();
+	}
+
+	public Collection<K> valueCollection() {
+		return map.valueCollection();
+	}
+
+	public K[] values() {
+		return map.values();
+	}
+}
