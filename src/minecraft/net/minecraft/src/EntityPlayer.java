efd5e61edfee71aaeb1fa1a408df5882bf023968
Wulfspider <hello@wulfspider.com>
2012-06-23 22:06:16 -0400

    Fix compile error for special particles.

--- src/minecraft/net/minecraft/src/EntityPlayer.java
+++ src/minecraft/net/minecraft/src/EntityPlayer.java
@@ -1251,7 +1251,7 @@ public abstract class EntityPlayer extends EntityLiving {
 		}
 
 		if (vip != null && rand.nextInt(4) == 0) {
-			Minecraft.theMinecraft.renderGlobal.func_40193_b(vip.getParticle(), posX + rand.nextFloat() - 0.5, boundingBox.minY + Math.max(0, rand.nextFloat() - 0.25F), posZ + rand.nextFloat() - 0.5, 0, 0, 0.0F);
+			Minecraft.theMinecraft.renderGlobal.func_40193_b(vip.getParticles(), posX + rand.nextFloat() - 0.5, boundingBox.minY + Math.max(0, rand.nextFloat() - 0.25F), posZ + rand.nextFloat() - 0.5, 0, 0, 0.0F);
 		}
 	}
 	//Spout End
