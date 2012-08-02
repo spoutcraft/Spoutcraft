bbabe863c70c37e88ef30553f50bb1ddb61e503b
alta189 <me@alta189.com>
2012-08-02 17:41:26 -0400

    ItemBlock update ItemMonsterPlacer update

--- src/minecraft/net/minecraft/src/ItemBlock.java
+++ src/minecraft/net/minecraft/src/ItemBlock.java
@@ -1,5 +1,7 @@
 package net.minecraft.src;
 
+import java.util.List;
+
 import com.pclewis.mcpatcher.mod.Colorizer;  //Spout HD
 
 public class ItemBlock extends Item {
@@ -15,11 +17,11 @@ public class ItemBlock extends Item {
 		return this.blockID;
 	}
 
-	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7) {
-		int var8 = par3World.getBlockId(par4, par5, par6);
-		if (var8 == Block.snow.blockID) {
+	public boolean func_77648_a(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
+		int var11 = par3World.getBlockId(par4, par5, par6);
+		if (var11 == Block.snow.blockID) {
 			par7 = 1;
-		} else if (var8 != Block.vine.blockID && var8 != Block.tallGrass.blockID && var8 != Block.deadBush.blockID) {
+		} else if (var11 != Block.vine.blockID && var11 != Block.tallGrass.blockID && var11 != Block.deadBush.blockID) {
 			if (par7 == 0) {
 				--par5;
 			}
@@ -51,15 +53,15 @@ public class ItemBlock extends Item {
 			return false;
 		} else if (par5 == 255 && Block.blocksList[this.blockID].blockMaterial.isSolid()) {
 			return false;
-		} else if (par3World.canBlockBePlacedAt(this.blockID, par4, par5, par6, false, par7)) {
-			Block var9 = Block.blocksList[this.blockID];
+		} else if (par3World.func_72931_a(this.blockID, par4, par5, par6, false, par7, par2EntityPlayer)) {
+			Block var12 = Block.blocksList[this.blockID];
 			if (par3World.setBlockAndMetadataWithNotify(par4, par5, par6, this.blockID, this.getMetadata(par1ItemStack.getItemDamage()))) {
 				if (par3World.getBlockId(par4, par5, par6) == this.blockID) {
-					Block.blocksList[this.blockID].onBlockPlaced(par3World, par4, par5, par6, par7);
+					Block.blocksList[this.blockID].func_71909_a(par3World, par4, par5, par6, par7, par8, par9, par10);
 					Block.blocksList[this.blockID].onBlockPlacedBy(par3World, par4, par5, par6, par2EntityPlayer);
 				}
 
-				par3World.playSoundEffect((double)((float)par4 + 0.5F), (double)((float)par5 + 0.5F), (double)((float)par6 + 0.5F), var9.stepSound.getStepSound(), (var9.stepSound.getVolume() + 1.0F) / 2.0F, var9.stepSound.getPitch() * 0.8F);
+				par3World.playSoundEffect((double)((float)par4 + 0.5F), (double)((float)par5 + 0.5F), (double)((float)par6 + 0.5F), var12.stepSound.getStepSound(), (var12.stepSound.getVolume() + 1.0F) / 2.0F, var12.stepSound.getPitch() * 0.8F);
 				--par1ItemStack.stackSize;
 			}
 
@@ -69,12 +71,54 @@ public class ItemBlock extends Item {
 		}
 	}
 
+	public boolean func_77884_a(World par1World, int par2, int par3, int par4, int par5, EntityPlayer par6EntityPlayer, ItemStack par7ItemStack) {
+		int var8 = par1World.getBlockId(par2, par3, par4);
+
+		if (var8 == Block.snow.blockID) {
+			par5 = 1;
+		} else if (var8 != Block.vine.blockID && var8 != Block.tallGrass.blockID && var8 != Block.deadBush.blockID) {
+			if (par5 == 0) {
+				--par3;
+			}
+
+			if (par5 == 1) {
+				++par3;
+			}
+
+			if (par5 == 2) {
+				--par4;
+			}
+
+			if (par5 == 3) {
+				++par4;
+			}
+
+			if (par5 == 4) {
+				--par2;
+			}
+
+			if (par5 == 5) {
+				++par2;
+			}
+		}
+
+		return par1World.func_72931_a(this.getBlockID(), par2, par3, par4, false, par5, (Entity)null);
+	}
+
 	public String getItemNameIS(ItemStack par1ItemStack) {
 		return Block.blocksList[this.blockID].getBlockName();
 	}
 
 	public String getItemName() {
 		return Block.blocksList[this.blockID].getBlockName();
+	}
+
+	public CreativeTabs func_77640_w() {
+		return Block.blocksList[this.blockID].func_71882_w();
+	}
+
+	public void func_77633_a(int par1, CreativeTabs par2CreativeTabs, List par3List) {
+		Block.blocksList[this.blockID].func_71879_a(par1, par2CreativeTabs, par3List);
 	}
 	
 //Spout start
--- src/minecraft/net/minecraft/src/ItemMonsterPlacer.java
+++ src/minecraft/net/minecraft/src/ItemMonsterPlacer.java
@@ -1,16 +1,21 @@
 package net.minecraft.src;
 
-import com.pclewis.mcpatcher.mod.Colorizer;  //Spout HD
+import com.pclewis.mcpatcher.mod.Colorizer;  // Spout HD
+
+import java.util.Iterator;
+import java.util.List;
 
 public class ItemMonsterPlacer extends Item {
 	public ItemMonsterPlacer(int par1) {
 		super(par1);
 		this.setHasSubtypes(true);
+		this.func_77637_a(CreativeTabs.field_78026_f);
 	}
 
 	public String getItemDisplayName(ItemStack par1ItemStack) {
 		String var2 = ("" + StatCollector.translateToLocal(this.getItemName() + ".name")).trim();
 		String var3 = EntityList.getStringFromID(par1ItemStack.getItemDamage());
+
 		if (var3 != null) {
 			var2 = var2 + " " + StatCollector.translateToLocal("entity." + var3 + ".name");
 		}
@@ -23,28 +28,32 @@ public class ItemMonsterPlacer extends Item {
 		return var3 != null ? (par2 == 0 ? Colorizer.colorizeSpawnerEgg(var3.primaryColor, par1, par2) : Colorizer.colorizeSpawnerEgg(var3.secondaryColor, par1, par2)) : Colorizer.colorizeSpawnerEgg(16777215, par1, par2); //Spout HD
 	}
 
-	public boolean func_46058_c() {
+	public boolean requiresMultipleRenderPasses() {
 		return true;
 	}
 
-	public int func_46057_a(int par1, int par2) {
-		return par2 > 0?super.func_46057_a(par1, par2) + 16:super.func_46057_a(par1, par2);
+	/**
+	 * Gets an icon index based on an item's damage value and the given render pass
+	 */
+	public int getIconFromDamageForRenderPass(int par1, int par2) {
+		return par2 > 0 ? super.getIconFromDamageForRenderPass(par1, par2) + 16 : super.getIconFromDamageForRenderPass(par1, par2);
 	}
 
-	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7) {
+	public boolean func_77648_a(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
 		if (par3World.isRemote) {
 			return true;
 		} else {
-			int var8 = par3World.getBlockId(par4, par5, par6);
+			int var11 = par3World.getBlockId(par4, par5, par6);
 			par4 += Facing.offsetsXForSide[par7];
 			par5 += Facing.offsetsYForSide[par7];
 			par6 += Facing.offsetsZForSide[par7];
-			double var9 = 0.0D;
-			if (par7 == 1 && var8 == Block.fence.blockID || var8 == Block.netherFence.blockID) {
-				var9 = 0.5D;
+			double var12 = 0.0D;
+
+			if (par7 == 1 && var11 == Block.fence.blockID || var11 == Block.netherFence.blockID) {
+				var12 = 0.5D;
 			}
 
-			if (func_48440_a(par3World, par1ItemStack.getItemDamage(), (double)par4 + 0.5D, (double)par5 + var9, (double)par6 + 0.5D) && !par2EntityPlayer.capabilities.isCreativeMode) {
+			if (spawnCreature(par3World, par1ItemStack.getItemDamage(), (double)par4 + 0.5D, (double)par5 + var12, (double)par6 + 0.5D) && !par2EntityPlayer.capabilities.isCreativeMode) {
 				--par1ItemStack.stackSize;
 			}
 
@@ -52,18 +61,40 @@ public class ItemMonsterPlacer extends Item {
 		}
 	}
 
-	public static boolean func_48440_a(World par0World, int par1, double par2, double par4, double par6) {
+	/**
+	 * Spawns the creature specified by the egg's type in the location specified by the last three parameters. Parameters:
+	 * world, entityID, x, y, z.
+	 */
+	public static boolean spawnCreature(World par0World, int par1, double par2, double par4, double par6) {
 		if (!EntityList.entityEggs.containsKey(Integer.valueOf(par1))) {
 			return false;
 		} else {
 			Entity var8 = EntityList.createEntityByID(par1, par0World);
+
 			if (var8 != null) {
 				var8.setLocationAndAngles(par2, par4, par6, par0World.rand.nextFloat() * 360.0F, 0.0F);
+
+				if (var8 instanceof EntityVillager) {
+					EntityVillager var9 = (EntityVillager)var8;
+					var9.setProfession(var9.getRNG().nextInt(5));
+					par0World.spawnEntityInWorld(var9);
+					return true;
+				}
+
 				par0World.spawnEntityInWorld(var8);
 				((EntityLiving)var8).playLivingSound();
 			}
 
 			return var8 != null;
+		}
+	}
+
+	public void func_77633_a(int par1, CreativeTabs par2CreativeTabs, List par3List) {
+		Iterator var4 = EntityList.entityEggs.values().iterator();
+
+		while (var4.hasNext()) {
+			EntityEggInfo var5 = (EntityEggInfo)var4.next();
+			par3List.add(new ItemStack(par1, 1, var5.spawnedID));
 		}
 	}
 }
