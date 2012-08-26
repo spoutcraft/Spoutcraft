8094d9a1b326a2e85f30d21775fbcd3b126de381
Zloteanu Nikita <teapa011@gmail.com>
2012-08-26 04:56:40 -0700

    Fixed some more rendering bugs!
    Signed-off by Zloteanu Nikita<teapa011@gmail.com>

--- src/minecraft/net/minecraft/src/RenderLiving.java
+++ src/minecraft/net/minecraft/src/RenderLiving.java
@@ -295,16 +295,10 @@ public class RenderLiving extends Render {
 		//Spout End
 	}
 
-	//Spout start
-	protected void renderLivingLabel(EntityLiving var1, String var2, double var3, double var5, double var7, int var9) {
-		renderLivingLabel(var1, var2, var3, var5, var7, var9, 0xFFFFFF, -1);
-	}
-
 	/**
 	 * Draws the debug or playername text above a living
 	 */
-	protected void renderLivingLabel(EntityLiving par1EntityLiving, String par2Str, double par3, double par5, double par7, int par9, int color, int color2) {
-	//Spout end
+	protected void renderLivingLabel(EntityLiving par1EntityLiving, String par2Str, double par3, double par5, double par7, int par9) {
 		double var10 = par1EntityLiving.getDistanceSqToEntity(this.renderManager.livingPlayer);
 
 		if (var10 <= (double)(par9 * par9)) {
@@ -320,7 +314,7 @@ public class RenderLiving extends Render {
 			GL11.glDisable(GL11.GL_LIGHTING);
 			GL11.glDepthMask(false);
 			GL11.glDisable(GL11.GL_DEPTH_TEST);
-			GL11.glDisable(GL11.GL_ALPHA_TEST); //Spout
+			GL11.glDisable(GL11.GL_ALPHA_TEST); //Spout - ?
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			Tessellator var15 = Tessellator.instance;
@@ -341,6 +335,7 @@ public class RenderLiving extends Render {
 			var15.draw();
 			GL11.glEnable(GL11.GL_TEXTURE_2D);
 			var12.drawString(par2Str, -var12.getStringWidth(par2Str) / 2, var16, 553648127);
+			GL11.glEnable(GL11.GL_ALPHA_TEST); //Spout - ?
 			GL11.glEnable(GL11.GL_DEPTH_TEST);
 			GL11.glDepthMask(true);
 			var12.drawString(par2Str, -var12.getStringWidth(par2Str) / 2, var16, -1);
