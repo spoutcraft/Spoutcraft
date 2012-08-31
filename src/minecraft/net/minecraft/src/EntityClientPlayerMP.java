161efb198c6bf1a978fb61c77405530cef18a96f
Zloteanu Nikita <teapa011@gmail.com>
2012-08-31 12:35:54 -0700

    Fixed rubberbanding and chunks not loading near the player!
    Signed-off-by: Zloteanu Nikita <teapa011@gmail.com>

--- src/minecraft/net/minecraft/src/EntityClientPlayerMP.java
+++ src/minecraft/net/minecraft/src/EntityClientPlayerMP.java
@@ -1,24 +1,33 @@
 package net.minecraft.src;
 
 import net.minecraft.client.Minecraft;
-//Spout Start
+
+//Spout start
 import org.spoutcraft.client.SpoutClient;
-import org.spoutcraft.client.packet.*;
+import org.spoutcraft.client.packet.PacketKeyPress;
 import org.spoutcraft.spoutcraftapi.gui.ScreenType;
-//Spout End
+//Spout end
 
 public class EntityClientPlayerMP extends EntityPlayerSP {
 	public NetClientHandler sendQueue;
 	private double oldPosX;
+
+	/** Old Minimum Y of the bounding box */
 	private double oldMinY;
 	private double oldPosY;
 	private double oldPosZ;
 	private float oldRotationYaw;
 	private float oldRotationPitch;
+
+	/** Check if was on ground last update */
 	private boolean wasOnGround = false;
+
+	/** should the player stop sneaking? */
 	private boolean shouldStopSneaking = false;
 	private boolean wasSneaking = false;
 	private int field_71168_co = 0;
+
+	/** has the client player's health been set? */
 	private boolean hasSetHealth = false;
 
 	public EntityClientPlayerMP(Minecraft par1Minecraft, World par2World, Session par3Session, NetClientHandler par4NetClientHandler) {
@@ -26,18 +35,27 @@ public class EntityClientPlayerMP extends EntityPlayerSP {
 		this.sendQueue = par4NetClientHandler;
 	}
 
+	/**
+	 * Called when the entity is attacked.
+	 */
 	public boolean attackEntityFrom(DamageSource par1DamageSource, int par2) {
 		return false;
 	}
 
+	/**
+	 * Heal living entity (param: amount of half-hearts)
+	 */
 	public void heal(int par1) {}
 
+	/**
+	 * Called to update the entity's position/logic.
+	 */
 	public void onUpdate() {
-		if(this.worldObj.blockExists(MathHelper.floor_double(this.posX), 0, MathHelper.floor_double(this.posZ))) {
+		if (this.worldObj.blockExists(MathHelper.floor_double(this.posX), 0, MathHelper.floor_double(this.posZ))) {
 			super.onUpdate();
 			this.sendMotionUpdates();
 		}
-		
+
 		//Spout start
 		if (!this.isSprinting() && runToggle) {
 			if (canSprint()) {
@@ -48,11 +66,14 @@ public class EntityClientPlayerMP extends EntityPlayerSP {
 		//Spout end
 	}
 
+	/**
+	 * Send updated motion and position information to the server
+	 */
 	public void sendMotionUpdates() {
 		boolean var1 = this.isSprinting();
 
-		if(var1 != this.wasSneaking) {
-			if(var1) {
+		if (var1 != this.wasSneaking) {
+			if (var1) {
 				this.sendQueue.addToSendQueue(new Packet19EntityAction(this, 4));
 			} else {
 				this.sendQueue.addToSendQueue(new Packet19EntityAction(this, 5));
@@ -62,8 +83,9 @@ public class EntityClientPlayerMP extends EntityPlayerSP {
 		}
 
 		boolean var2 = this.isSneaking();
+
 		if (var2 != this.shouldStopSneaking) {
-			if(var2) {
+			if (var2) {
 				this.sendQueue.addToSendQueue(new Packet19EntityAction(this, 1));
 			} else {
 				this.sendQueue.addToSendQueue(new Packet19EntityAction(this, 2));
@@ -74,24 +96,23 @@ public class EntityClientPlayerMP extends EntityPlayerSP {
 
 		double var3 = this.posX - this.oldPosX;
 		double var5 = this.boundingBox.minY - this.oldMinY;
-
-		double var7 = this.posY - this.oldPosY;
+		double var7 = this.posZ - this.oldPosZ;
 		double var9 = (double)(this.rotationYaw - this.oldRotationYaw);
 		double var11 = (double)(this.rotationPitch - this.oldRotationPitch);
 		boolean var13 = var3 * var3 + var5 * var5 + var7 * var7 > 9.0E-4D || this.field_71168_co >= 20;
 		boolean var14 = var9 != 0.0D || var11 != 0.0D;
 
-		if(this.ridingEntity != null) {
-			if(var14) {
+		if (this.ridingEntity != null) {
+			if (var14) {
 				this.sendQueue.addToSendQueue(new Packet11PlayerPosition(this.motionX, -999.0D, -999.0D, this.motionZ, this.onGround));
 			} else {
 				this.sendQueue.addToSendQueue(new Packet13PlayerLookMove(this.motionX, -999.0D, -999.0D, this.motionZ, this.rotationYaw, this.rotationPitch, this.onGround));
 			}
 
 			var13 = false;
-		} else if(var13 && var14) {
+		} else if (var13 && var14) {
 			this.sendQueue.addToSendQueue(new Packet13PlayerLookMove(this.posX, this.boundingBox.minY, this.posY, this.posZ, this.rotationYaw, this.rotationPitch, this.onGround));
-		} else if(var13) {
+		} else if (var13) {
 			this.sendQueue.addToSendQueue(new Packet11PlayerPosition(this.posX, this.boundingBox.minY, this.posY, this.posZ, this.onGround));
 		} else if (var14) {
 			this.sendQueue.addToSendQueue(new Packet12PlayerLook(this.rotationYaw, this.rotationPitch, this.onGround));
@@ -100,9 +121,9 @@ public class EntityClientPlayerMP extends EntityPlayerSP {
 		}
 
 		++this.field_71168_co;
-
 		this.wasOnGround = this.onGround;
-		if(var13) {
+
+		if (var13) {
 			this.oldPosX = this.posX;
 			this.oldMinY = this.boundingBox.minY;
 			this.oldPosY = this.posY;
@@ -110,23 +131,35 @@ public class EntityClientPlayerMP extends EntityPlayerSP {
 			this.field_71168_co = 0;
 		}
 
-		if(var14) {
+		if (var14) {
 			this.oldRotationYaw = this.rotationYaw;
 			this.oldRotationPitch = this.rotationPitch;
 		}
 	}
 
+	/**
+	 * Called when player presses the drop item key
+	 */
 	public EntityItem dropOneItem() {
 		this.sendQueue.addToSendQueue(new Packet14BlockDig(4, 0, 0, 0, 0));
 		return null;
 	}
 
+	/**
+	 * Joins the passed in entity item with the world. Args: entityItem
+	 */
 	protected void joinEntityItemWithWorld(EntityItem par1EntityItem) {}
 
+	/**
+	 * Sends a chat message from the player. Args: chatMessage
+	 */
 	public void sendChatMessage(String par1Str) {
 		this.sendQueue.addToSendQueue(new Packet3Chat(par1Str));
 	}
 
+	/**
+	 * Swings the item the player is holding.
+	 */
 	public void swingItem() {
 		super.swingItem();
 		this.sendQueue.addToSendQueue(new Packet18Animation(this, 1));
@@ -136,19 +169,27 @@ public class EntityClientPlayerMP extends EntityPlayerSP {
 		this.sendQueue.addToSendQueue(new Packet205ClientCommand(1));
 	}
 
-	public void damageEntity(DamageSource par1DamageSource, int par2) { //Spout protected -> public
+	/**
+	 * Deals damage to the entity. If its a EntityPlayer then will take damage from the armor first and then health second
+	 * with the reduced value. Args: damageAmount
+	 */
+	public void damageEntity(DamageSource par1DamageSource, int par2) { //Spout - public
 		this.setEntityHealth(this.getHealth() - par2);
-		//Spout start
-		GuiChat.interruptChat();
-		//Spout end
+		GuiChat.interruptChat(); //Spout
 	}
 
+	/**
+	 * sets current screen to null (used on escape buttons of GUIs)
+	 */
 	public void closeScreen() {
 		this.sendQueue.addToSendQueue(new Packet101CloseWindow(this.craftingInventory.windowId));
 		this.inventory.setItemStack((ItemStack)null);
 		super.closeScreen();
 	}
 
+	/**
+	 * Updates health locally.
+	 */
 	public void setHealth(int par1) {
 		if (this.hasSetHealth) {
 			super.setHealth(par1);
@@ -158,20 +199,25 @@ public class EntityClientPlayerMP extends EntityPlayerSP {
 		}
 	}
 
+	/**
+	 * Adds a value to a statistic field.
+	 */
 	public void addStat(StatBase par1StatBase, int par2) {
-		if(par1StatBase != null) {
-			if(par1StatBase.isIndependent) {
+		if (par1StatBase != null) {
+			if (par1StatBase.isIndependent) {
 				super.addStat(par1StatBase, par2);
 			}
 		}
 	}
 
+	/**
+	 * Used by NetClientHandler.handleStatistic
+	 */
 	public void incrementStat(StatBase par1StatBase, int par2) {
-		if(par1StatBase != null) {
-			if(!par1StatBase.isIndependent) {
+		if (par1StatBase != null) {
+			if (!par1StatBase.isIndependent) {
 				super.addStat(par1StatBase, par2);
 			}
-
 		}
 	}
 
