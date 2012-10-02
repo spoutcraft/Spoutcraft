a52504b898322ae89b704da3c35e09b3ea35fbfe
Afforess <afforess@gmail.com>
2012-10-01 23:52:37 -0400

    Revert broken attempted fix

--- src/main/java/net/minecraft/src/GuiScreen.java
+++ src/main/java/net/minecraft/src/GuiScreen.java
@@ -1022,9 +1022,6 @@ public class GuiScreen extends Gui
 		if (!wasSandboxed) {
 			SpoutClient.enableSandbox();
 		}
-		
-		//TODO this should not be needed
-		widget.setScreen(getScreen());
 
 		int left = (int) widget.getActualX();
 		int top = (int) widget.getActualY();
