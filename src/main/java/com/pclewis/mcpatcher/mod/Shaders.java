28cfc16004503798a30a1fd53ad77d68d89adc57
Wulfspider <hello@wulfspider.com>
2012-10-07 01:04:26 -0400

    Commented OpenGL log message, as it's a bit spammy... - This needs to be looked into as it shouldn't be used at all when shaders are disabled as far as I know.

--- src/main/java/com/pclewis/mcpatcher/mod/Shaders.java
+++ src/main/java/com/pclewis/mcpatcher/mod/Shaders.java
@@ -88,8 +88,8 @@ public class Shaders {
 	private static boolean enabled= true;
 
 	public static void setup(int type) {
-		System.out.println("OpenGL Version: " + GL11.glGetString(GL11.GL_VERSION));
-		System.out.println("OpenGL Platform: " + GL11.glGetString(GL11.GL_VENDOR));
+		//System.out.println("OpenGL Version: " + GL11.glGetString(GL11.GL_VERSION));
+		//System.out.println("OpenGL Platform: " + GL11.glGetString(GL11.GL_VENDOR));
 
 		if(!(enabled)) return;
 		mc = Minecraft.theMinecraft;
