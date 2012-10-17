3292a5db8a07866f51b9a6fa908747045133fc87
Afforess <afforess@gmail.com>
2012-10-17 14:50:54 -0300

    Add LWJGL version

--- src/main/java/org/spoutcraft/client/gui/error/GuiUnexpectedError.java
+++ src/main/java/org/spoutcraft/client/gui/error/GuiUnexpectedError.java
@@ -35,6 +35,7 @@ import net.minecraft.client.ClientBrandRetriever;
 import net.minecraft.client.Minecraft;
 import net.minecraft.src.GuiScreen;
 
+import org.lwjgl.Sys;
 import com.pclewis.mcpatcher.mod.Shaders;
 import com.pclewis.mcpatcher.mod.TextureUtils;
 import com.pclewis.mcpatcher.mod.TileSize;
@@ -138,6 +139,7 @@ public class GuiUnexpectedError extends GuiScreen {
 			builder.append("    Texture Pack: ").append(TextureUtils.getTexturePackName(TextureUtils.getSelectedTexturePack())).append("\n");
 			builder.append("    Texture Pack Res: ").append(TileSize.int_size + "x").append("\n");
 			builder.append("    Shaders: ").append(Boolean.toString(Shaders.isEnabled())).append("\n");
+			builder.append("    LWJGL Version: ").append(Sys.getVersion()).append("\n");
 
 			builder.append("System Information:\n");
 			builder.append("    Operating System: ").append(System.getProperty("os.name")).append("\n");
