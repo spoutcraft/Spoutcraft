0ee4377e902762135635c5185177f9e3797a479b
Chris Churchwell <chrisrulesall@gmail.com>
2012-10-17 02:04:52 -0500

    The gui
    Signed-off-by: Chris Churchwell <chrisrulesall@gmail.com>

---
+++ src/main/java/org/spoutcraft/client/gui/precache/GuiPrecache.java
@@ -0,0 +1,32 @@
+package org.spoutcraft.client.gui.precache;
+
+import net.minecraft.src.GuiScreen;
+
+import org.spoutcraft.api.Spoutcraft;
+import org.spoutcraft.api.addon.Addon;
+import org.spoutcraft.api.gui.GenericLabel;
+import org.spoutcraft.api.gui.WidgetAnchor;
+
+public class GuiPrecache extends GuiScreen {
+	
+	public GenericLabel statusText;
+	
+	@Override
+	public void initGui() {
+		
+		Addon spoutcraft = Spoutcraft.getAddonManager().getAddon("Spoutcraft");
+		
+		statusText = new GenericLabel();
+		statusText.setAnchor(WidgetAnchor.CENTER_CENTER);
+		statusText.setAlign(WidgetAnchor.CENTER_CENTER);
+		statusText.setText("Downloading Textures...");
+		
+		getScreen().attachWidget(spoutcraft, statusText);
+	}
+	
+	@Override
+	public void drawScreen(int par1, int par2, float par3) {
+		this.drawBackground(0);
+		super.drawScreen(par1, par2, par3);
+	}
+}
