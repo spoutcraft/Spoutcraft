a6436cb91ed7b72186a33da7f98d0fccbed27289
Chris Sanders <zidane@outlook.com>
2012-11-29 17:11:58 -0600

    Cleanup. Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spoutcraft/client/packet/PacketValidatePrecache.java
+++ src/main/java/org/spoutcraft/client/packet/PacketValidatePrecache.java
@@ -1,27 +1,34 @@
+/*
+ * This file is part of Spoutcraft.
+ *
+ * Copyright (c) 2011-2012, Spout LLC <http://www.spout.org/>
+ * Spoutcraft is licensed under the GNU Lesser General Public License.
+ *
+ * Spoutcraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Spoutcraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.spoutcraft.client.packet;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map.Entry;
-
-import net.java.games.util.plugins.Plugin;
 
 import org.spoutcraft.api.io.SpoutInputStream;
 import org.spoutcraft.api.io.SpoutOutputStream;
-import org.spoutcraft.client.SpoutClient;
-import org.spoutcraft.client.gui.precache.GuiPrecache;
-import org.spoutcraft.client.io.FileUtil;
 import org.spoutcraft.client.precache.PrecacheManager;
 import org.spoutcraft.client.precache.PrecacheTuple;
 
 public class PacketValidatePrecache implements SpoutPacket {
-	
 	int count;
 	PrecacheTuple[] plugins;
-	
-	public PacketValidatePrecache() {
-	}
 	
 	@Override
 	public void readData(SpoutInputStream input) throws IOException {
@@ -49,7 +56,6 @@ public class PacketValidatePrecache implements SpoutPacket {
 
 	@Override
 	public void run(int playerId) {
-		
 		PrecacheManager.showPreloadGui();
 		
 		PrecacheManager.reset();
@@ -80,5 +86,4 @@ public class PacketValidatePrecache implements SpoutPacket {
 	public int getVersion() {
 		return 0;
 	}
-
 }
