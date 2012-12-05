bd37c533e3345bfa030b686a76ba69c36f6ec83b
Mike Howe <mike@mcsnetworks.com>
2012-12-04 22:58:48 -0600

    Fix overlay touching check boxes when creating a new single player world.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/org/spoutcraft/client/gui/singleplayer/GuiCreateWorld.java
+++ src/main/java/org/spoutcraft/client/gui/singleplayer/GuiCreateWorld.java
@@ -164,7 +164,7 @@ public class GuiCreateWorld extends GuiSpoutScreen {
 		labelWorldType.setX(fleft).setY(ftop + 6).setWidth(200).setHeight(20);
 		comboWorldType.setX(fright).setY(ftop).setWidth(200).setHeight(20);
 
-		ftop += 25;
+		ftop += 58;
 
 		checkHardcore.setX(fright - 50).setY(ftop).setWidth(200).setHeight(20);
 
