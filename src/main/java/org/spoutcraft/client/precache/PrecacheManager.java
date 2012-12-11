b0d89271055da19e268e57b2c532da21c537adbe
Chris Sanders <zidane@outlook.com>
2012-12-11 16:50:46 -0600

    Need to look this over a bit more, not 100%.
    Revert "Cleaned up caching with help from @Dockter."
    
    This reverts commit 3d00cc5aab1409fa7d059e66af9b4856a5c1d2ab.

--- src/main/java/org/spoutcraft/client/precache/PrecacheManager.java
+++ src/main/java/org/spoutcraft/client/precache/PrecacheManager.java
@@ -31,10 +31,10 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
 import net.minecraft.client.Minecraft;
-
 import org.bukkit.ChatColor;
 
 import org.spoutcraft.api.block.design.GenericBlockDesign;
@@ -47,11 +47,12 @@ import org.spoutcraft.client.io.FileUtil;
 import org.spoutcraft.client.packet.PacketRequestPrecache;
 
 public class PrecacheManager {
+	
 	/**
 	 * Tuple plugin -> boolean isCached
 	 */
 	public static HashMap<PrecacheTuple, Boolean> plugins = new HashMap<PrecacheTuple, Boolean>();
-
+	
 	/**
 	 * Adds a plugin tuple to the hashmap, and checks if its cached and valid
 	 * @param plugin
@@ -67,23 +68,23 @@ public class PrecacheManager {
 				isCached = true;
 			}
 		}
-
+		
 		System.out.println("Plugin Precache: " +
 				plugin.getPlugin() + " " +
 				plugin.getVersion() + " || " +
-				"isCached= " + String.valueOf(isCached) +
+				"isCached= "+ String.valueOf(isCached)+
 				" || ServerCRC= " + String.valueOf(plugin.getCrc()) + " " +
 				"ClientCRC= " + String.valueOf(crc));
 		plugins.put(plugin, isCached);
 	}
-
+	
 	/**
 	 * resets the plugins. Useful to clear out previous entries when starting a new login sequence.
 	 */
 	public static void reset() {
 		plugins.clear();
 	}
-
+	
 	/**
 	 * Returns the tuple that matches to parameters
 	 * @param plugin
@@ -91,14 +92,14 @@ public class PrecacheManager {
 	 */
 	public static PrecacheTuple getPrecacheTuple(String plugin, String version) {
 		for (Entry entry : plugins.entrySet()) {
-			PrecacheTuple tuple = (PrecacheTuple) entry.getKey();
-			if (tuple.getPlugin().equalsIgnoreCase(plugin) && tuple.getVersion().equalsIgnoreCase(version)) {
+			PrecacheTuple tuple = (PrecacheTuple)entry.getKey();
+			if(tuple.getPlugin().equalsIgnoreCase(plugin) && tuple.getVersion().equalsIgnoreCase(version)) {
 				return tuple;
 			}
 		}
 		return null;
 	}
-
+	
 	/**
 	 * Sets the given plugin precache tuple to a cached status.
 	 * @param plugin
@@ -108,33 +109,31 @@ public class PrecacheManager {
 			plugins.put(plugin, true);
 		}
 	}
-
+	
 	/**
 	 * Checks if there is a plugin precache still needing to be cached.
 	 * @return true if a plugin needs to be cached, false is everything is already cached.
 	 */
 	public static boolean hasNextCache() {
-		for (boolean cached : plugins.values()) {
-			if (!cached) {
-				return true;
-			}
+		for( boolean cached : plugins.values()) {
+			if (cached == false) return true;
 		}
 		return false;
 	}
-
+	
 	/**
 	 * gets the next item to be cached
 	 * @return
 	 */
 	public static PrecacheTuple getNextToCache() {
-		for (Entry entry : plugins.entrySet()) {
-			if (!((Boolean) entry.getValue()).booleanValue()) {
-				return (PrecacheTuple) entry.getKey();
+		for( Entry entry : plugins.entrySet()) {
+			if ( ((Boolean)entry.getValue()).booleanValue() == false ) {
+				return (PrecacheTuple)entry.getKey();
 			}
 		}
 		return null;
 	}
-
+	
 	/**
 	 * Starts the next cache
 	 */
@@ -143,96 +142,113 @@ public class PrecacheManager {
 			loadPrecache(true);
 			return;
 		}
-
+		
 		PrecacheTuple next = getNextToCache();
-
+		
 		if (next == null) {
 			loadPrecache(true);
 			return;
 		}
-
-		setPreloadGuiText(ChatColor.BLUE + "Spoutcraft" + "\n" + " " + "\n" + ChatColor.WHITE + "Downloading Custom Content for:  " + ChatColor.ITALIC + next.getPlugin() + " " + next.getVersion());
-
+		
+		setPreloadGuiText(ChatColor.BLUE + "Spoutcraft" + "\n"+" "+ "\n"+ ChatColor.WHITE + "Downloading Custom Content for:  " + ChatColor.ITALIC + next.getPlugin() + " " + next.getVersion());
+		
 		SpoutClient.getInstance().getPacketManager().sendSpoutPacket(new PacketRequestPrecache(next.getPlugin()));
+		
 	}
-
+	
 	public static File getPluginPreCacheFile(PrecacheTuple plugin) {
 		return getPluginPreCacheFile(plugin.getPlugin(), plugin.getVersion());
 	}
-
+	
 	public static File getPluginPreCacheFile(String plugin, String version) {
-		return new File(FileUtil.getCacheDir(), plugin + "_" + version + ".zip");
+		return new File(FileUtil.getCacheDir(), plugin+"_"+version+".zip");
 	}
-
-	public static void loadPrecache(boolean reloadRenderer) {
-
+	
+	public static void loadPrecache(boolean reloadRenderer) {		
+		
 		//unzip
 		File cacheRoot = FileUtil.getCacheDir();
-		for (Entry entry : plugins.entrySet()) {
+		
+		for(Entry entry : plugins.entrySet()) {
 			try {
-				PrecacheTuple plugin = (PrecacheTuple) entry.getKey();
-				boolean isCached = (Boolean) entry.getValue();
-
-				final ZipFile zip = new ZipFile(getPluginPreCacheFile(plugin));
-				final Enumeration zipEntries = zip.entries();
-
-				while (zipEntries.hasMoreElements()) {
-					final ZipEntry zipEntry = (ZipEntry) zipEntries.nextElement();
-					if (zipEntry.isDirectory()) {
-						continue;
-					}
-					final File target = new File(cacheRoot, plugin.getPlugin() + "/" + zipEntry.getName());
-					if (!isCached) {
+				PrecacheTuple plugin = (PrecacheTuple)entry.getKey();
+				boolean isCached = (Boolean)entry.getValue();
+				
+				if(isCached == true) {
+					ZipFile zip = new ZipFile(getPluginPreCacheFile(plugin));
+					Enumeration zipEntries = zip.entries();
+					
+					while (zipEntries.hasMoreElements())
+					{
+						ZipEntry zipEntry = (ZipEntry) zipEntries.nextElement();
+						if (zipEntry.isDirectory()) {
+							continue;
+						}
+						File target = new File(cacheRoot, plugin.getPlugin() + "/" + zipEntry.getName());
+						
+						if (target.exists() && zipEntry.getCrc() == FileUtil.getCRC(target, new byte[(int) target.length()])) {
+							continue;
+						}
+						
+						//at this point if it exists its not a vlid crc
 						if (target.exists()) {
 							target.delete();
 						}
-
+						
 						//parent folder, not target
-						if (!target.getParentFile().exists()) {
+						if(!target.getParentFile().exists()) {
 							target.getParentFile().mkdirs();
 						}
-
-						final BufferedInputStream is = new BufferedInputStream(zip.getInputStream(zipEntry));
+						
+						BufferedInputStream is = new BufferedInputStream(zip.getInputStream(zipEntry));
 						int currentByte;
-						final int BUFFER_SIZE = 100 * 1024;
-						final byte[] buf = new byte[BUFFER_SIZE];
-
-						final BufferedOutputStream dest = new BufferedOutputStream(new FileOutputStream(target), BUFFER_SIZE);
-
-						while ((currentByte = is.read(buf, 0, BUFFER_SIZE)) != -1) {
+						byte[] buf = new byte[1024];
+						
+						FileOutputStream fos = new FileOutputStream(target);
+						BufferedOutputStream dest = new BufferedOutputStream(fos, 1024);
+						
+						while ((currentByte = is.read(buf, 0, 1024)) != -1) {
 							dest.write(buf, 0, currentByte);
 						}
 						dest.flush();
 						dest.close();
 						is.close();
-					}
-					if (target.exists()) {
-						if (target.getName().endsWith(".sbd")) {
+						
+						if (target.exists() && target.getName().endsWith(".sbd")) {
 							loadDesign(target);
-						} else if (FileUtil.isImageFile(target.getName())) {
+						}
+						else if (target.exists() && FileUtil.isImageFile(target.getName())) {
 							CustomTextureManager.getTextureFromUrl(plugin.getPlugin(), target.getName());
 						}
+						
 					}
+					
+					zip.close();
 				}
-				zip.close();
-			} catch (Exception e) {
+			} catch (ZipException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		if (reloadRenderer) {
+		
+		if (reloadRenderer == true) {
 			if (Minecraft.theMinecraft.theWorld != null) {
 				Minecraft.theMinecraft.renderGlobal.updateAllRenderers();
 			}
 		}
+		
 		closePreloadGui();
 	}
-
+	
 	public static void showPreloadGui() {
 		//display precache gui.
 		SpoutClient.getHandle().displayGuiScreen(new GuiPrecache(), false);
 		setPreloadGuiText("Checking Plugin Caches...");
 	}
-
+	
 	public static void closePreloadGui() {
 		if (SpoutClient.getHandle().currentScreen instanceof GuiPrecache) {
 			// Closes downloading terrain
@@ -241,26 +257,30 @@ public class PrecacheManager {
 			SpoutClient.getHandle().clearPreviousScreen();
 		}
 	}
-
+	
 	public static void setPreloadGuiText(String text) {
 		if (SpoutClient.getHandle().currentScreen instanceof GuiPrecache) {
-			((GuiPrecache) SpoutClient.getHandle().currentScreen).statusText.setText(text);
-			((GuiPrecache) SpoutClient.getHandle().currentScreen).statusText.onTick();
+			((GuiPrecache)SpoutClient.getHandle().currentScreen).statusText.setText(text);
+			((GuiPrecache)SpoutClient.getHandle().currentScreen).statusText.onTick();
 		}
 	}
-
+	
 	public static void loadDesign(File file) {
 		short customId = -1;
 		byte data = 0;
-		GenericBlockDesign design = new GenericBlockDesign();
-
+		GenericBlockDesign design = null;
+		
 		try {
-			final DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
+			DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
 			customId = in.readShort();
 			data = in.readByte();
-			design = new GenericBlockDesign();
+			design = new GenericBlockDesign();	
 			design.read(in);
-		} catch (Exception e) {
+			
+		} catch (EOFException e) {
+			//data input stream always throws EOFException when complete
+			//e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			if (design != null && design.isReset()) {
