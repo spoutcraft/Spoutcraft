cdb7cfe21a9afa80b0f4240bd8970d3723fa14b9
Mike Howe <mike@mcsnetworks.com>
2012-12-29 02:09:26 -0600

    2nd fix for Color Code Space calculations.  This time it will properly remove formatting and colors from the calculation.  This fixes GenericText Widgets and Over-head PlayerName and Title alignment issues.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/pclewis/mcpatcher/mod/FontUtils.java
+++ src/main/java/com/pclewis/mcpatcher/mod/FontUtils.java
@@ -117,7 +117,7 @@ public class FontUtils {
 		float var2 = 0.0F;
 
 		if (var1 != null) {
-			var1 = var1.replaceAll("(?i)\u00A7[0-F]", ""); //Spout - Strip ordinary colors
+			var1 = org.bukkit.ChatColor.stripColor(var1); // Spout - Strip Colors & Formatting Codes to calculate spacing properly.
 			boolean var3 = false;
 
 			for (int var4 = 0; var4 < var1.length(); ++var4) {
