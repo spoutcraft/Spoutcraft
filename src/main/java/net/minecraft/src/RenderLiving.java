92a3e58386b8c9774b93232979f9d5106edb4b64
Mike Howe <dockter@almuramc.com>
2013-04-11 21:49:00 -0500

    Fix for PlayerName rendering in inventory window.  Fix for VIP Player name NOT rendering as specified VIP name.  Fix for PlayerName rendering when Invisibility potion is applied.
    Signed-off-by: Mike Howe <dockter@almuramc.com>

--- src/main/java/net/minecraft/src/RenderLiving.java
+++ src/main/java/net/minecraft/src/RenderLiving.java
@@ -9,6 +9,9 @@ import com.prupe.mcpatcher.mod.MobRandomizer;
 // MCPatcher End
 // Spout Start
 import org.spoutcraft.client.SpoutClient;
+import org.spoutcraft.client.player.accessories.AccessoryHandler;
+import org.spoutcraft.client.player.accessories.AccessoryType;
+import org.spoutcraft.client.special.VIP;
 // Spout End
 
 public class RenderLiving extends Render {
@@ -357,15 +360,93 @@ public class RenderLiving extends Render {
 	 * Passes the specialRender and renders it
 	 */
 	protected void passSpecialRender(EntityLiving par1EntityLiving, double par2, double par4, double par6) {
-		// Spout Start
-		if (Minecraft.isDebugInfoEnabled() && SpoutClient.getInstance().isEntityLabelCheat()) {
-			this.renderLivingLabel(par1EntityLiving, Integer.toString(par1EntityLiving.entityId), par2, par4, par6, 64);
-		} else {
-			String title = par1EntityLiving.displayName;
-			if (title != null && !title.equals("[hide]")) {
-				String lines[] = title.split("\\n");
-				for (int i = 0; i < lines.length; i++){
-					renderLivingLabel(par1EntityLiving, lines[i], par2, par4 + (0.275D * (lines.length - i - 1)), par6, 64);
+		// Spout Start - Include renderName method here previously located in RenderPlayer.
+		if (!(par1EntityLiving instanceof EntityPlayer)) { // Renderer for Entities since they can have a custom name (wolves for example)
+			if (Minecraft.isDebugInfoEnabled() && SpoutClient.getInstance().isEntityLabelCheat()) {
+				this.renderLivingLabel(par1EntityLiving, Integer.toString(par1EntityLiving.entityId), par2, par4, par6, 64);
+			} else {
+				String title = par1EntityLiving.displayName;
+				if (title != null && !title.equals("[hide]")) {
+					String lines[] = title.split("\\n");
+					for (int i = 0; i < lines.length; i++){
+						renderLivingLabel(par1EntityLiving, lines[i], par2, par4 + (0.275D * (lines.length - i - 1)), par6, 64);
+					}
+				}
+			}
+		} else if (par1EntityLiving instanceof EntityPlayer) {
+			EntityPlayer par1EntityPlayer = (EntityPlayer) par1EntityLiving;
+			if (!par1EntityPlayer.getHasActivePotion()) {
+				if(Minecraft.isGuiEnabled() && (par1EntityPlayer != this.renderManager.livingPlayer || (Minecraft.theMinecraft.gameSettings.thirdPersonView != 0 && Minecraft.theMinecraft.currentScreen == null))) {
+					float var8 = 1.6F;
+					float var9 = 0.016666668F * var8;
+					double var10 = par1EntityPlayer.getDistanceSqToEntity(this.renderManager.livingPlayer);
+					float var12 = par1EntityPlayer.isSneaking() ? 32.0F : 64.0F;
+
+					if (var10 < (double)(var12 * var12)) {
+						String title = null;
+						VIP vip = par1EntityPlayer.vip;
+						if (vip != null) {
+							title = vip.getTitle();
+						} else {
+							title = par1EntityPlayer.displayName;
+						}
+						float alpha = 0.25F;
+						
+						if (!title.equals("[hide]")) {
+							String lines[] = title.split("\\n");
+							double y = par4;
+							for (int line = 0; line < lines.length; line++) {
+								title = lines[line];
+								par4 = y + (0.275D * (lines.length - line - 1));
+
+								if (AccessoryHandler.hasAccessory(par1EntityPlayer.username, AccessoryType.NOTCHHAT)) {
+									par4 = par4 + 0.275d;
+								} else if (AccessoryHandler.hasAccessory(par1EntityPlayer.username, AccessoryType.TOPHAT)) {
+									par4 = par4 + 0.5d;
+								}
+
+								if (!par1EntityPlayer.isSneaking()) {
+									if (par1EntityPlayer.isPlayerSleeping()) {
+										this.renderLivingLabel(par1EntityPlayer, title, par2, par4 - 1.5D, par6, 64);
+									} else {
+										this.renderLivingLabel(par1EntityPlayer, title, par2, par4, par6, 64);
+									}
+								} else {
+									title = org.bukkit.ChatColor.stripColor(title); //strip colors when sneaking
+									FontRenderer var14 = this.getFontRendererFromRenderManager();
+									GL11.glPushMatrix();
+									GL11.glTranslatef((float)par2 + 0.0F, (float)par4 + 2.3F, (float)par6);
+									GL11.glNormal3f(0.0F, 1.0F, 0.0F);
+									GL11.glRotatef(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+									GL11.glRotatef(this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+									GL11.glScalef(-var9, -var9, var9);
+									GL11.glDisable(GL11.GL_LIGHTING);
+									GL11.glTranslatef(0.0F, 0.25F / var9, 0.0F);
+									GL11.glDepthMask(false);
+
+									GL11.glEnable(GL11.GL_BLEND);
+									GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+									Tessellator var15 = Tessellator.instance;
+									GL11.glDisable(GL11.GL_TEXTURE_2D);
+									var15.startDrawingQuads();
+									int var16 = var14.getStringWidth(title) / 2;
+									var15.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
+									var15.addVertex((double)(-var16 - 1), -1.0D, 0.0D);
+									var15.addVertex((double)(-var16 - 1), 8.0D, 0.0D);
+									var15.addVertex((double)(var16 + 1), 8.0D, 0.0D);
+									var15.addVertex((double)(var16 + 1), -1.0D, 0.0D);
+									var15.draw();
+									GL11.glEnable(GL11.GL_TEXTURE_2D);
+									GL11.glDepthMask(true);
+									var14.drawString(title, -var14.getStringWidth(title) / 2, 0, 553648127);
+									GL11.glEnable(GL11.GL_LIGHTING);
+									GL11.glDisable(GL11.GL_BLEND);
+									GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+									GL11.glPopMatrix();
+								}
+							}
+						}
+					}
 				}
 			}
 		}
